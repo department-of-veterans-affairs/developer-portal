0dbd254dd051bd91907a5aa144a4e31356377614
Julia Elman <julia@adhocteam.us>
2018-12-08 15:35:09 -0500

    Adding declaration for Formation megamenu.

--- @department-of-veterans-affairs/formation/index.d.ts
+++ @department-of-veterans-affairs/formation/index.d.ts
@@ -1,4 +1,5 @@
 declare module '@department-of-veterans-affairs/formation/AlertBox';
 declare module '@department-of-veterans-affairs/formation/ErrorableTextArea';
 declare module '@department-of-veterans-affairs/formation/ErrorableTextInput';
+declare module '@department-of-veterans-affairs/formation/Megamenu';
 declare module '@department-of-veterans-affairs/formation/ProgressButton';
