7e6befd4b0326419a481ea9c29920f01fe13ac02
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2018-12-19 12:52:41 -0600

    Add threshold to diff

--- config/jest/imageSnapshot.ts
+++ config/jest/imageSnapshot.ts
@@ -1,5 +1,8 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 
-const toMatchImageSnapshot = configureToMatchImageSnapshot({});
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  failureThreshold: 1,
+  failureThresholdType: 'percent', // Considers <=1% difference to be passing
+});
 
 expect.extend({ toMatchImageSnapshot });
