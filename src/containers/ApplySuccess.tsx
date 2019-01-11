996eb877ed7097a2cfe2d77224ef76f932d09e76
Drew Vogel <drew@adhocteam.us>
2019-01-11 17:25:32 -0600

    Change post-apply feedback to be closer to follow-up email. (#881)

--- src/containers/ApplySuccess.tsx
+++ src/containers/ApplySuccess.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 import { connect } from 'react-redux';
 
-import { IApplication, IRootState } from '../types';
+import sentenceJoin from '../sentenceJoin';
+import { IApiList, IApplication, IRootState } from '../types';
 import { ASSISTANCE_EMAIL_ADDRESS } from '../types/constants';
 
 import './Apply.scss';
@@ -27,16 +28,60 @@ function AssistanceTrailer() {
   );
 }
 
-function TokenNotice({ token } : { token: string }) {
-  return (
-    <div>
-      <p className="usa-font-lead"><strong>Your VA API token is:</strong> {token}</p>
-    </div>
-  );
+interface IApiKeyNoticeProps {
+  token: string;
+  email: string;
+  selectedApis: string[];
+}
+
+class ApiKeyNotice extends React.Component<IApiKeyNoticeProps, {}> {
+  private apisToProperNames = {
+    appeals: 'Appeals Status API',
+    benefits: 'Benefits Intake API',
+    facilities: 'VA Facilities API',
+    health: 'Health API',
+    verification: 'Veteran Verification API',
+  };
+
+  public render() {
+    const { token, email, selectedApis } = this.props;
+    const apiListSnippet = this.apisToEnglishList(selectedApis.filter((k) => k !== 'health'));
+
+    return (
+      <div>
+        <p className="usa-font-lead"><strong>Your VA API key is:</strong> {token}</p>
+
+        <p>
+        You should receive an email at {email} with the same key. That key is for accessing the {apiListSnippet} in the development environment.
+        You can use it by including it as an HTTP request header in each request to the API:
+        </p>
+
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;<pre style={{display: 'inline'}}>apiKey: {token}</pre></p>
+      </div>
+    );
+  }
+
+  private apisToEnglishList(apis: string[]): string {
+    return sentenceJoin(apis.map((k) => {
+      return this.apisToProperNames[k];
+    }));
+  }
+}
+
+function selectedApiNames(apis: IApiList): string[] {
+  return Object.keys(apis).filter((apiName) => {
+    return apis[apiName] === true;
+  });
 }
 
 function ApplySuccess(props: IApplication) {
   const { inputs: { apis, email }, token } = props;
+  const selectedApis=selectedApiNames(apis);
+  const onlyOAuthSelected = ((selectedApis.length === 1) && (selectedApis[0] === "health"));
+
+  const tokenNotice = onlyOAuthSelected
+    ? null
+    : <ApiKeyNotice email={email.value} token={token} selectedApis={selectedApiNames(apis)} />;
 
   const oAuthNotice = (apis.health)
     ? <span>We will email you at {email.value} soon to setup OAuth credentials for the Health API.</span>
@@ -44,15 +89,12 @@ function ApplySuccess(props: IApplication) {
 
   return (
     <div role="region" aria-labelledby="apply-region" className="usa-grid api-application">
-      <strong>Thank you for signing up!</strong>
+      <p><strong>Thank you for signing up!</strong></p>
 
-      <TokenNotice token={token} />
+      {tokenNotice}
 
-      <p>
-        This same token will also be emailed to {email.value}. You can use this
-        token now in the dev environment. It will work with all APIs other than
-        the Health API. {oAuthNotice}
-      </p>
+      {oAuthNotice}
+
       <AssistanceTrailer />
     </div>
   );
