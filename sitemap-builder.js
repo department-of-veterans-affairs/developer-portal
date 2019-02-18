c696e91f0bf90cbded45384591808a4a921e16f8
Nick Fasulo <nrfasulo@gmail.com>
2019-02-18 14:49:44 -0600

    Create sitemap with static routes

---
+++ sitemap-builder.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+require('@babel/register')({
+    presets: [ '@babel/env', '@babel/react' ],
+});
+
+const paths = require('./config/paths');
+const router = require('./src/sitemap-routes.jsx').default;
+const Sitemap = require('react-router-sitemap').default;
+
+// building paths from dynamic routes
+// will need to import apiDefs.ts to further build out dynamic paths
+const paramsConfig = {
+    '/explore/:apiCategoryKey': [
+        { apiCategoryKey: ['benefits', 'facilities', 'health', 'verification' ] },
+      ]
+};
+
+const sitemap = (
+    new Sitemap(router)
+        .applyParams(paramsConfig)
+        .build(paths.publicUrl)
+        .save("./sitemap.xml")
+);
