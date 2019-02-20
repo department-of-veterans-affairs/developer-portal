64911f2ac346018e7fe7363d4cef520b30df038d
Nick Fasulo <nrfasulo@gmail.com>
2019-02-20 09:57:44 -0600

    Add sitemap update to build script

--- scripts/build.js
+++ scripts/build.js
@@ -25,6 +25,7 @@ const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
+const buildSitemap = require('../sitemap-builder');
 
 const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
