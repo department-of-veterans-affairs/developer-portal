56fc03239e0a6515f4ce0752c81275031414ef18
Nick Fasulo <nrfasulo@gmail.com>
2019-02-25 16:06:52 -0600

    Revert "Add sitemap update to build script"
    This reverts commit 64911f2ac346018e7fe7363d4cef520b30df038d.

--- scripts/build.js
+++ scripts/build.js
@@ -25,7 +25,6 @@ const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
-const buildSitemap = require('../sitemap-builder');
 
 const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
