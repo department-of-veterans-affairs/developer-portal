74cadb0df231c5df1aeeb3eeadc79ea709fc9b98
Mike Lumetta <mikelumetta@Mike-Lumettas-MacBook-Pro.local>
2019-03-05 12:55:44 -0500

    Fix lint issue - #1157

--- src/components/ApiCard.test.tsx
+++ src/components/ApiCard.test.tsx
@@ -8,7 +8,7 @@ describe('ApiCard', () => {
   it('renders the name', () => {
     const apiCard = shallow(
       <ApiCard name="Special API" description="Use this to manage something!" url="/special"
-        vaInternalOnly={false} />
+        vaInternalOnly={false} />,
     );
     expect(apiCard.find('h3.va-api-name').length).toBe(1);
     expect(apiCard.find('h3.va-api-name').text()).toBe('Special API');
@@ -17,7 +17,7 @@ describe('ApiCard', () => {
   it('renders the description', () => {
     const apiCard = shallow(
       <ApiCard name="Special API" description="Use this to manage something!" url="/special"
-        vaInternalOnly={false} />
+        vaInternalOnly={false} />,
     );
     expect(apiCard.find('.va-api-description').length).toBe(1);
     expect(apiCard.find('.va-api-description').text()).toBe('Use this to manage something!');
@@ -26,13 +26,13 @@ describe('ApiCard', () => {
   it('includes the VA internal tag based on the truthiness of the vaInternalOnly prop', () => {
     let apiCard = shallow(
       <ApiCard name="Special API" description="Use this to manage something!" url="/special"
-        vaInternalOnly={false} />
+        vaInternalOnly={false} />,
     );
     expect(apiCard.find('.va-internal-tag').length).toBe(0);
 
     apiCard = shallow(
       <ApiCard name="Special API" description="Use this to manage something!" url="/special"
-        vaInternalOnly={true} />
+        vaInternalOnly={true} />,
     );
     expect(apiCard.find('.va-internal-tag').length).toBe(1);
     expect(apiCard.find('.va-internal-tag').text()).toBe('Internal VA use only');
