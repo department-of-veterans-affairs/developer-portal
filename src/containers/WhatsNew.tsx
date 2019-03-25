2d49d64cc60334a4c8c332c02499f39f50b70777
Nick Fasulo <nrfasulo@gmail.com>
2019-03-25 12:11:08 -0500

    Add SideNav

--- src/containers/WhatsNew.tsx
+++ src/containers/WhatsNew.tsx
@@ -1,15 +1,42 @@
 import * as React from 'react';
+import { NavHashLink } from 'react-router-hash-link';
+
+import { NavLink } from 'react-router-dom';
 
 import WhatsNewPage from '../content/whats-new.mdx';
 
+export function SideNav() {
+
+  return (
+    <ul role="navigation" aria-label="Release Notes Side Nav" className="usa-sidenav-list">
+      <li key="all">
+        <NavLink exact={true} to="/whats-new" className="side-nav-category-link" activeClassName="usa-current">
+          Overview
+        </NavLink>
+        <NavHashLink className="side-nav-category-link" activeClassName="usa-current" id="hash-link-va-press-releases" to="#va-press-releases">
+          VA Press Releases
+        </NavHashLink>
+        <NavHashLink className="side-nav-category-link" activeClassName="usa-current" id="hash-link-news" to="#news">
+          News
+        </NavHashLink>
+        <NavHashLink className="side-nav-category-link" activeClassName="usa-current" id="hash-link-media" to="#media">
+          Media
+        </NavHashLink>
+      </li>
+    </ul>
+  );
+}
+
 export class WhatsNew extends React.Component {
+  private navRef = React.createRef<HTMLDivElement>();
+
   public render() {
     return (
       <div className="Explore">
         <section className="usa-section">
           <div className="Explore-main usa-grid">
-            <div className="vadp-side-nav usa-width-one-third sticky">
-              SideNav
+            <div className="vadp-side-nav usa-width-one-third sticky" ref={this.navRef}>
+              <SideNav />
             </div>
             <div className="usa-width-two-thirds">
               <WhatsNewPage />
