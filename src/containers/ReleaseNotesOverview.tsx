beaac894b33fca9f8780853df017d8654c14cfa8
Nick Fasulo <nrfasulo@gmail.com>
2019-03-28 10:38:59 -0500

    Pull cards rendering out into a function

--- src/containers/ReleaseNotesOverview.tsx
+++ src/containers/ReleaseNotesOverview.tsx
@@ -5,24 +5,28 @@ import { apiCategoryOrder, apiDefs } from '../apiDefs';
 import ApiCard from '../components/ApiCard';
 import PageHeader from '../components/PageHeader';
 
+function cards(parent: string): any {
+  return apiCategoryOrder.map((apiCategoryKey: string) => {
+    const { name, shortDescription } = apiDefs[apiCategoryKey];
+    const cardUrl = parent + '/' + apiCategoryKey;
+    let apiCard;
+    if (apiDefs[apiCategoryKey].releaseNotes) {
+      apiCard = (
+        <ApiCard name={name} description={shortDescription} key={apiCategoryKey} vaInternalOnly={false}
+          url={cardUrl} />
+      );
+    }
+    return apiCard;
+  });
+}
+
 export default class Overview extends React.Component<RouteComponentProps & any, {}> {
-  public render() { 
+  public render() {
     return (
       <div className="overview">
         <PageHeader halo={this.props.halo} header={this.props.header} description={this.props.description} />
         <div className="va-api-container">
-          {apiCategoryOrder.map((apiCategoryKey: string) => {
-            const { name, shortDescription } = apiDefs[apiCategoryKey];
-            const cardUrl = this.props.parent + '/' + apiCategoryKey;
-            let apiCard;
-            if (apiDefs[apiCategoryKey].releaseNotes) {
-              apiCard = (
-                <ApiCard name={name} description={shortDescription} key={apiCategoryKey} vaInternalOnly={false}
-                  url={cardUrl} />
-              );
-            }
-            return apiCard;
-          })}
+          {cards(this.props.parent)}
         </div>
       </div>
     );
