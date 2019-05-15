b338a2bbc16d1a20ff3adfddc442823b8391acea
Josh Salisbury <josh@adhocteam.us>
2019-05-15 10:38:54 -0500

    Decrease pixel difference sensitivity for visual regression tests

--- config/jest/imageSnapshot.ts
+++ config/jest/imageSnapshot.ts
@@ -1,5 +1,7 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 
-const toMatchImageSnapshot = configureToMatchImageSnapshot({});
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+    customDiffConfig: { threshold: 0.03 },
+});
 
 expect.extend({ toMatchImageSnapshot });
