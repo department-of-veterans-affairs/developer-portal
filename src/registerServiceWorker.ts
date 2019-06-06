dd86026f0036ef222de6274df6c6a8244703f6cb
Josh Salisbury <josh@adhocteam.us>
2019-06-06 14:54:18 -0500

    Revert "Update workbox webpack plugin"
    This reverts commit 0758f6d0cc4f68496f5f033a4656012f0f866f71.

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -70,13 +70,7 @@ function registerValidSW(swUrl: string) {
                 // the fresh content will have been added to the cache.
                 // It's the perfect time to display a 'New content is
                 // available; please refresh.' message in your web app.
-                if(window.confirm('New content is available, press okay to refresh.')) {
-                  navigator.serviceWorker.addEventListener('controllerchange', (event) => {
-                    window.location.reload();
-                  });
-
-                  installingWorker.postMessage({ type: 'SKIP_WAITING', meta: 'test' });
-                }
+                console.log('New content is available; please refresh.');
               } else {
                 // At this point, everything has been precached.
                 // It's the perfect time to display a
