ce278963863f0efc3f8411a3832515a69fb3131b
ATeal <arteal89@gmail.com>
2019-06-18 09:51:28 -0400

    prettier

--- src/base.accessibility.ts
+++ src/base.accessibility.ts
@@ -1,10 +1,10 @@
-import { axeCheck, mockSwagger, puppeteerHost, testPaths } from "./e2eHelpers";
+import { axeCheck, mockSwagger, puppeteerHost, testPaths } from './e2eHelpers';
 
-describe("Accessibility tests", async () => {
+describe('Accessibility tests', async () => {
   for (const path of testPaths) {
     // Disabled until we can actually fix these failures. Some are caused by swagger-ui,
     // so unlikely to be fixable before then
-    if (path === "/apply") {
+    if (path === '/apply') {
       continue;
     }
 
@@ -12,12 +12,12 @@ describe("Accessibility tests", async () => {
       // Mock swagger requests on docs pages so those pages aren't blank
       if (/^\/explore\/[^\/]+\/docs/.test(path)) {
         await page.setRequestInterception(true);
-        page.removeAllListeners("request");
-        page.on("request", mockSwagger);
+        page.removeAllListeners('request');
+        page.on('request', mockSwagger);
       }
 
-      await page.goto(`${puppeteerHost}${path}`, { waitUntil: "networkidle0" });
-      await page.addScriptTag({ path: require.resolve("axe-core") });
+      await page.goto(`${puppeteerHost}${path}`, { waitUntil: 'networkidle0' });
+      await page.addScriptTag({ path: require.resolve('axe-core') });
       const result = await page.evaluate(axeCheck);
       expect(result).toHaveNoViolations();
     });
