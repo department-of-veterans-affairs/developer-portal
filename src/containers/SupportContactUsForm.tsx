80406231fc7b62dc43334fbf866e6c64622873e4
Will Huang <52384927+whuang85@users.noreply.github.com>
2019-08-16 15:46:26 -0700

    update form to match new mock

--- src/containers/SupportContactUsForm.tsx
+++ src/containers/SupportContactUsForm.tsx
@@ -1,6 +1,7 @@
 import ErrorableTextArea from '@department-of-veterans-affairs/formation/ErrorableTextArea';
 import ErrorableTextInput from '@department-of-veterans-affairs/formation/ErrorableTextInput';
 import ProgressButton from '@department-of-veterans-affairs/formation/ProgressButton';
+import * as classNames from 'classnames';
 import * as React from "react";
 import { validateEmail } from '../actions';
 import { IErrorableInput } from '../types';
@@ -18,6 +19,8 @@ export interface ISupportContactUsFormState {
 
 interface IAPIValue {
   benefits: boolean;
+  claims: boolean;
+  communityCare: boolean;
   facilities: boolean;
   health: boolean;
   verification: boolean;
@@ -35,6 +38,8 @@ export default class SupportContactUsForm extends React.Component<ISupportContac
     this.state = {
       apis: {
         benefits: false,
+        claims: false,
+        communityCare: false,
         facilities: false,
         health: false,
         verification: false,
@@ -114,49 +119,71 @@ export default class SupportContactUsForm extends React.Component<ISupportContac
 
           <p>If applicable, please select any of the APIs pertaining to your issue.</p>
 
-          <div className="usa-grid va-api-checkboxes">
+          <h3>Standard APIs:</h3>
+
           <div className="form-checkbox">
-                <input
-                  type="checkbox"
-                  id="benefits"
-                  name="benefits"
-                  checked={this.state.apis.benefits}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
-                <label htmlFor="benefits">Benefits</label>
-              </div>
-
-              <div className="form-checkbox">
-                <input
-                  type="checkbox"
-                  id="health"
-                  name="health"
-                  checked={this.state.apis.health}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
-                <label htmlFor="health">VA Health API</label>
-              </div>
-
-              <div className="form-checkbox">
-                <input
-                  type="checkbox"
-                  id="facilities"
-                  name="facilities"
-                  checked={this.state.apis.facilities}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)}/>
-                <label htmlFor="facilities">Facilities</label>
-              </div>
-
-              <div className="form-checkbox">
-                <input
-                  type="checkbox"
-                  id="verification"
-                  name="verification"
-                  checked={this.state.apis.verification}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
-                <label htmlFor="verification">Veteran Verification</label>
-              </div>
+            <input
+              type="checkbox"
+              id="benefits"
+              name="benefits"
+              checked={this.state.apis.benefits}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="benefits">VA Benefits API</label>
           </div>
 
-          <div className="usa-grid va-description">
+          <div className="form-checkbox">
+            <input
+              type="checkbox"
+              id="facilities"
+              name="facilities"
+              checked={this.state.apis.facilities}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="facilities">VA Facilities API</label>
+          </div>
+
+          <h3>OAuth APIs:</h3>
+
+          <div className="form-checkbox">
+            <input
+              type="checkbox"
+              id="claims"
+              name="claims"
+              checked={this.state.apis.claims}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="claims">VA Claims API</label>
+          </div>
+
+          <div className="form-checkbox">
+            <input
+              type="checkbox"
+              id="health"
+              name="health"
+              checked={this.state.apis.health}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="health">VA Health API</label>
+          </div>
+
+          <div className="form-checkbox">
+            <input
+              type="checkbox"
+              id="communityCare"
+              name="communityCare"
+              checked={this.state.apis.communityCare}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="communityCare">Community Care Eligibility API</label>
+          </div>
+
+          <div className="form-checkbox">
+            <input
+              type="checkbox"
+              id="verification"
+              name="verification"
+              checked={this.state.apis.verification}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.toggleApis(event)} />
+            <label htmlFor="verification">VA Veteran Verification API</label>
+          </div>
+
+          <div className={classNames("usa-grid", "va-description")}>
             <div className="usa-width-one-whole">
               <ErrorableTextArea
                 errorMessage={null}
