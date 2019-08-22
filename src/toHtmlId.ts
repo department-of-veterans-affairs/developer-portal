78b6a5417e465138da8c509c111159762ba1d996
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2019-08-22 12:34:34 -0500

    Strip out characters disallowed in a CSS id

--- src/toHtmlId.ts
+++ src/toHtmlId.ts
@@ -3,7 +3,7 @@ function toHtmlId(phrase: string) {
   return phrase
     .replace(/^[^a-zA-Z]+/, '') // Ensure id starts with a letter
     .replace(/\s+/g, '-')
-    .replace(/[^\w-_'?!()*]+/g, '')
+    .replace(/[^\w-_]+/g, '');
 }
 
 export default toHtmlId;
