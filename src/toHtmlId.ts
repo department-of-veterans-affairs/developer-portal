26fa489f10fa1829590afb8c15d64d3b78a12211
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2019-08-22 09:58:36 -0500

    Make toHtmlId a little more strict

--- src/toHtmlId.ts
+++ src/toHtmlId.ts
@@ -1,6 +1,9 @@
 // Converts a string to an html id suitable for use in an anchor link
 function toHtmlId(phrase: string) {
-  return phrase.replace(/\s+/g, '-').toLowerCase()
+  return phrase
+    .replace(/^[^a-zA-Z]+/, '') // Ensure id starts with a letter
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-_'?!()*]+/g, '')
 }
 
 export default toHtmlId;
