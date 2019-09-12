84d5ee63f64cee1058f4b2589191ca2ac59248e3
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2019-09-11 23:40:22 -0500

    Move hashMatch function to public instance method

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -8,44 +8,44 @@ import * as Stickyfill from 'stickyfilljs';
 
 import '../components/SideNav.scss';
 
-interface ISideNavEntryProps extends NavHashLinkProps {
+export interface ISideNavEntryProps extends NavHashLinkProps {
   name: string | JSX.Element;
   to: string; // Only allow the string form of the `to` prop
-}
-
-function withoutTrailingSlash(path: string) {
-  if (path.length === 1) {
-    return path;
-  }
-  return path.replace(/\/$/, '');
 }
 
 // Constructs a NavHashLink in the sidebar that also takes into account the
 // hash when determining if it's active
 export class SideNavEntry extends React.Component<ISideNavEntryProps> {
-  public render() {
-    // Override the default activeCheck to also match on hash
-    const hashMatch = (_: match, location: Location): boolean => {
-      // Because we're not using the builtin check, we need our own logic
-      // to handle trailing slashes
-      const currentPath = withoutTrailingSlash(location.pathname);
-      const to = withoutTrailingSlash(this.props.to);
-
-      // Match on just the hash for anchor links with no path
-      const onlyHashMatches = location.hash && to.endsWith(location.hash);
-      if (this.props.exact) {
-        return onlyHashMatches || to === `${currentPath}${location.hash}`;
-      } else {
-        return onlyHashMatches || currentPath.startsWith(to);
+  // Override the default activeCheck to also match on hash
+  public hashMatch = (_: match, location: Location): boolean => {
+    // Because we're not using the builtin check, we need our own logic
+    // to handle trailing slashes
+    const withoutTrailingSlash = (path: string) => {
+      if (path.length === 1) {
+        return path;
       }
+      return path.replace(/\/$/, '');
     };
 
+    const currentPath = withoutTrailingSlash(location.pathname);
+    const to = withoutTrailingSlash(this.props.to);
+
+    // Match on just the hash for anchor links with no path
+    const onlyHashMatches = location.hash && to.endsWith(location.hash);
+    if (this.props.exact) {
+      return onlyHashMatches || to === `${currentPath}${location.hash}`;
+    } else {
+      return onlyHashMatches || currentPath.startsWith(to);
+    }
+  }; // tslint:disable-line: semicolon
+
+  public render() {
     // Omit unneeded parent props from NavLink
     const { name, ...navLinkProps } = this.props;
 
     return (
       <li>
-        <NavHashLink activeClassName="usa-current" isActive={hashMatch} {...navLinkProps}>
+        <NavHashLink activeClassName="usa-current" isActive={this.hashMatch} {...navLinkProps}>
           {this.props.name}
         </NavHashLink>
         {this.props.children && <ul className="usa-sidenav-sub_list">{this.props.children}</ul>}
