35a89722a9499a4e6b32f1f93df3e5398a0a18ae
Josh Salisbury <josh@adhocteam.us>
2019-09-25 16:24:33 -0500

    Add a bit of margin to the NotFound component
    This component will be replaced soon so not a lot of time was spent
    styling the component

--- src/containers/NotFound.tsx
+++ src/containers/NotFound.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 function NotFound() {
-  return <h1>404 Not Found</h1>;
+  return (
+    <div style={{marginLeft: "30px"}}>
+      <h1>404 Not Found</h1>
+    </div>
+  );
 }
 
 export default NotFound;
