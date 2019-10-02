d3632a1524c6a50a15b34577b1efc9f4e1b01f24
Drew Vogel <drew@adhocteam.us>
2019-10-02 16:12:36 -0400

    remove console.log

--- src/containers/documentation/ProviderIntegrationGuide.tsx
+++ src/containers/documentation/ProviderIntegrationGuide.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import IntegrationGuide from '../../content/providers/integrationGuide.mdx';
 
 export default function ProviderIntegrationGuide() {
-  console.log(IntegrationGuide);
   return (
       <section className="usa-section">
         <div className="usa-grid">
