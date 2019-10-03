79c3cf5a998add4cc2c16c71c73577ae2555b290
Philip Becker <philip.becker@oddball.io>
2019-10-03 13:31:15 -0400

    set final values

--- src/apiDefs/data/appeals.ts
+++ src/apiDefs/data/appeals.ts
@@ -1,17 +1,16 @@
 import { IApiDescription } from '../schema';
 
-// const swaggerHost : string = process.env.REACT_APP_VETSGOV_SWAGGER_API!;
+const swaggerHost : string = process.env.REACT_APP_VETSGOV_SWAGGER_API!;
 const appealsApis : IApiDescription[] = [
   {
     description: 'The Decision Reviews API allows you to interact with a Veteran’s Decision Review requests, also known as benefit appeals.',
     docSources: [
       {
         // metadataUrl: ,// metadata endpoint is not yet exposed
-        // openApiUrl: `${swaggerHost}/services/appeals/docs/v3/decision_reviews`,
-        openApiUrl: "http://localhost:3000/api/docs/v3/decision_reviews",
+        openApiUrl: `${swaggerHost}/services/appeals/docs/v3/decision_reviews`,
       },
     ],
-    enabledByDefault: true,
+    enabledByDefault: false,
     name: 'Decision Reviews API',
     urlFragment: 'decision_reviews',
     vaInternalOnly: true,
