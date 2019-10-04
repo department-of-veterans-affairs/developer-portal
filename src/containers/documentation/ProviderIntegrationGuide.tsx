0bd1b0d846489b8c1be0af5f8cb5901e60957e09
Drew Vogel <drew@adhocteam.us>
2019-10-04 12:02:22 -0400

    Go back to typescript for markdown module component.

--- src/containers/documentation/ProviderIntegrationGuide.tsx
+++ src/containers/documentation/ProviderIntegrationGuide.tsx
@@ -1,7 +1,3 @@
-// This module cannot use TypeScript because the compiler does not have enough
-// type information about the component returned by the markdown component
-// loader to understand that it should accept the standard `className`
-// attribute.
 import * as React from 'react';
 import IntegrationGuideContent from '../../content/providers/integrationGuide.mdx';
 
--- src/types/md.d.ts
+++ src/types/md.d.ts
@@ -1,4 +1,9 @@
 declare module "*.mdx" {
-    const MarkdownComponent: React.StatelessComponent;
-    export default MarkdownComponent;
+  // This type definition isn't entirely accurate. The underlying component would be happy to accept more
+  // attributes than this type signature will allow. However, the precise definition of those extra attributes
+  // is defined by the markdown-component-loader at runtime based on configuration data. This constrained type
+  // signature is used here as a statement of our preference to limit our use the component loader to it's
+  // basic functionality.
+  const MarkdownComponent: React.FunctionComponent<React.HTMLAttributes<HTMLElement>>;
+  export default MarkdownComponent;
 }
