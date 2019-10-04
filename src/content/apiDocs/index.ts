c9097e4ee2809d7c9b3c4fad90ffb69880d0d73a
Philip Becker <philip.becker@oddball.io>
2019-10-04 14:02:49 -0400

    use naming convention for release notes

--- src/content/apiDocs/index.ts
+++ src/content/apiDocs/index.ts
@@ -1,28 +1,28 @@
 import { IApiCategoryContent } from '../../apiDefs/schema';
-import AppealsReleaseNotes from './appeals/appeals.mdx';
 import AppealsIntro from './appeals/appealsIntro.mdx';
 import AppealsOverview from './appeals/appealsOverview.mdx';
 import AppealsQuickstart from './appeals/appealsQuickstart.mdx';
-import BenefitsReleaseNotes from './benefits/benefits.mdx';
+import AppealsReleaseNotes from './appeals/appealsReleaseNotes.mdx';
 import BenefitsIntro from './benefits/benefitsIntro.mdx';
 import BenefitsOverview from './benefits/benefitsOverview.mdx';
-import FacilitiesReleaseNotes from './facilities/facilities.mdx';
+import BenefitsReleaseNotes from './benefits/benefitsReleaseNotes.mdx';
 import FacilitiesIntro from './facilities/facilitiesIntro.mdx';
 import FacilitiesOverview from './facilities/facilitiesOverview.mdx';
+import FacilitiesReleaseNotes from './facilities/facilitiesReleaseNotes.mdx';
 import HealthArgonautPostDeprecation from './health/argonautDeprecatedNotice.mdx';
 import HealthArgonautPreDeprecation from './health/argonautDeprecationNotice.mdx';
 import CommunityCareApiIntro from './health/communityCareApiIntro.mdx';
 import FhirArgonautApiIntro from './health/fhirArgonautApiIntro.mdx';
 import FhirDSTU2ApiIntro from './health/fhirDSTU2ApiIntro.mdx';
 import FhirR4ApiIntro from './health/fhirR4ApiIntro.mdx';
-import HealthReleaseNotes from './health/health.mdx';
 import HealthIntro from './health/healthIntro.mdx';
 import HealthOverview from './health/healthOverview.mdx';
 import HealthQuickstart from './health/healthQuickstart.mdx';
+import HealthReleaseNotes from './health/healthReleaseNotes.mdx';
 import UrgentCareApiIntro from './health/urgentCareApiIntro.mdx';
-import VerificationReleaseNotes from './verification/verification.mdx';
 import VerificationIntro from './verification/verificationIntro.mdx';
 import VerificationOverview from './verification/verificationOverview.mdx';
+import VerificationReleaseNotes from './verification/verificationReleaseNotes.mdx';
 
 const appealsContent: IApiCategoryContent = {
   intro: AppealsIntro,
