d7c9632995d8029a11b17b68c2a897b6c9e2841f
Philip Becker <philip.becker@oddball.io>
2019-10-04 13:37:46 -0400

    minor fixes from PR feedback

--- src/content/releaseNotes/index.ts
+++ src/content/releaseNotes/index.ts
@@ -4,5 +4,11 @@ import FacilitiesReleaseNotes from './facilities.mdx';
 import HealthReleaseNotes from './health.mdx';
 import VerificationReleaseNotes from './verification.mdx';
 
-export { AppealsReleaseNotes, BenefitsReleaseNotes, FacilitiesReleaseNotes, HealthReleaseNotes, VerificationReleaseNotes };
+export {
+    AppealsReleaseNotes,
+    BenefitsReleaseNotes,
+    FacilitiesReleaseNotes,
+    HealthReleaseNotes,
+    VerificationReleaseNotes,
+};
 
