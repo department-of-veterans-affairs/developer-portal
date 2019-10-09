5bb00d23e0f2be95a818604589e00ad3e0c26ea4
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-10-08 22:47:24 -0400

    Remove all references to usa-section

--- src/components/MarkdownPage.tsx
+++ src/components/MarkdownPage.tsx
@@ -4,10 +4,12 @@ import * as React from 'react';
 
 export default function MarkdownPage(Component: MarkdownComponent): JSX.Element {
   return (
-    <section className="usa-section">
-      <div className={classNames('vads-l-grid-container', 'vads-u-padding-y--4')}>
-        <Component />
-      </div>
-    </section>
+    <div className={classNames(
+      'vads-u-width--full',
+      'vads-u-margin--2',
+      'medium-screen:vads-u-margin--4',
+    )}>
+      <Component />
+    </div>
   );
 }
