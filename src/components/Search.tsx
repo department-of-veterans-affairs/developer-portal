293bc9dbf67b465a94dffc163deb2ed3feff8d82
Josh Salisbury <josh@adhocteam.us>
2019-10-11 10:38:36 -0500

    Rename vadp styles to va-api

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,8 +9,8 @@ import './Search.scss';
 export default class Search extends React.Component<{inMenu?: boolean, className?: string}> {
   public render() {
     const { inMenu } = this.props;
-    const wrapperClassName = classNames("vadp-search-wrapper", this.props.className);
-    const wrapperClassNameModified = inMenu ? wrapperClassName : classNames(wrapperClassName, "vadp-search-form--inverse-color");
+    const wrapperClassName = classNames("va-api-search-wrapper", this.props.className);
+    const wrapperClassNameModified = inMenu ? wrapperClassName : classNames(wrapperClassName, "va-api-search-form--inverse-color");
     const inputClassName = inMenu ? classNames("usagov-search-autocomplete") : classNames("usagov-search-autocomplete", "vads-u-padding-left--4");
     const placeholderText = inMenu? "" : "Search...";
 
@@ -20,7 +20,7 @@ export default class Search extends React.Component<{inMenu?: boolean, className
         <form action="https://search.usa.gov/search"
             acceptCharset="UTF-8"
             method="get"
-            className="vadp-search-form">
+            className="va-api-search-form">
           <input name="utf8" type="hidden" value="&#x2713;" />
           <input type="hidden" name="affiliate" id="affiliate" value="developer.va.gov" />
           <input type="text"
@@ -31,7 +31,7 @@ export default class Search extends React.Component<{inMenu?: boolean, className
             placeholder={placeholderText}
             aria-label="Search developer.va.gov" />
           {inMenu &&
-          <button type="submit" name="commit" className="vadp-search-submit" value="Search" aria-label="Search">
+          <button type="submit" name="commit" className="va-api-search-submit" value="Search" aria-label="Search">
             <FontAwesomeIcon icon={faSearch} />
           </button>
           }
