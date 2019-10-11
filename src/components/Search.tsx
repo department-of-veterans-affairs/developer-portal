b696af1d15c945bbfccad8da68f7b4fc49a449b9
Josh Salisbury <josh@adhocteam.us>
2019-10-11 09:56:58 -0500

    Fix mobile menu styles
    The search box has different styles in the mobile menu nav

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,22 +6,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './Search.scss';
 
-export default class Search extends React.Component<{className?: string}> {
+export default class Search extends React.Component<{inMenu: boolean, className?: string}> {
+  public static defaultProps = {
+    inMenu: false,
+  };
+
   public render() {
+    const { inMenu } = this.props;
+    const wrapperClassName = classNames("vadp-search-wrapper", this.props.className);
+    const wrapperClassNameModified = inMenu ? wrapperClassName : classNames(wrapperClassName, "vadp-search-form--inverse-color");
+    const inputClassName = inMenu ? classNames("usagov-search-autocomplete") : classNames("usagov-search-autocomplete", "vads-u-padding-left--4");
+    const placeholderText = inMenu? "" : "Search...";
+
     return (
-      <div className="vadp-search-form">
-        <FontAwesomeIcon className="va-api-search-icon" icon={faSearch} />
+      <div className={wrapperClassNameModified}>
+        {!inMenu && <FontAwesomeIcon className="va-api-search-icon" icon={faSearch} />}
         <form action="https://search.usa.gov/search"
-            acceptCharset="UTF-8" method="get">
+            acceptCharset="UTF-8"
+            method="get"
+            className="vadp-search-form">
           <input name="utf8" type="hidden" value="&#x2713;" />
           <input type="hidden" name="affiliate" id="affiliate" value="developer.va.gov" />
           <input type="text"
             name="query"
             id="query"
             autoComplete="off"
-            className={classNames("usagov-search-autocomplete vads-u-padding-left--4")}
-            placeholder="Search..."
+            className={inputClassName}
+            placeholder={placeholderText}
             aria-label="Search developer.va.gov" />
+          {inMenu &&
+          <button type="submit" name="commit" className="vadp-search-submit" value="Search" aria-label="Search">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          }
         </form>
       </div>
     );
