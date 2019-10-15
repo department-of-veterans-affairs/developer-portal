7e1ea817342e72d6ba5f61ffb583363577ac8977
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-10-15 15:34:58 -0400

    Use VADS in VA-related components

--- src/components/VeteransCrisisLine.tsx
+++ src/components/VeteransCrisisLine.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { faComments, faDeaf, faMobileAlt, faPhone, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faComments, faDeaf, faMobileAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 
@@ -31,61 +31,64 @@ export default class VeteransCrisisLine extends React.Component<IVeteransCrisisL
   }
 
   public render() {
-    const modalClasses = classNames(
-      'va-modal', 
-      'va-modal-large', 
-      {
-        'va-overlay': !this.props.visible,
-        'va-overlay-open': this.props.visible,
-      });
+    const iconClasses = classNames('va-api-crisis-panel-icon', 'vads-u-margin-right--2');
+    const listItemClasses = classNames(
+      'vads-u-display--flex',
+      'vads-u-flex-wrap--nowrap',
+      'vads-u-align-items--center',
+      'vads-u-border-bottom--1px',
+      'vads-u-border-top--0',
+      'vads-u-border-color--gray-lighter',  
+    );
 
     return (
-      <div id="modal-crisisline" className={modalClasses} role="alertdialog">
-        <div className="va-crisis-panel va-modal-inner">
-          <button className="va-modal-close va-overlay-close" onClick={this.props.closeHandler} type="button" aria-label="Close this modal">
-            <FontAwesomeIcon icon={faTimesCircle} />
-          </button>
+      <div className={classNames('va-crisis-panel', 'va-api-crisis-panel', 'vads-u-max-width--none')}>
+        <div className="va-crisis-panel-body">
+          <h3 className={classNames('vads-u-display--flex', 'vads-u-align-items--flex-start')}>
+            <img src={vclLogo} alt="Veterans Crisis Line logo" aria-label="Veterans Crisis Line logo"
+              className={classNames('va-api-crisis-line-logo', 'vads-u-margin-top--1', 'vads-u-margin-right--1p5')} />
+            <span>We're here anytime, day or night - 24/7</span>
+          </h3>
+          <p>
+            If you are a Veteran in crisis or concerned about one, connect with our caring,
+            qualified responders for confidential help. Many of them are Veterans themselves.
+          </p>
+          <ul className={classNames(
+            'va-api-crisis-panel-list',
+            'vads-u-border-top--1px',
+            'vads-u-border-color--gray-lighter',
+            'vads-u-text-align--left',
+          )}>
+            <li className={listItemClasses}>
+              <FontAwesomeIcon icon={faPhone} className={iconClasses} />
+              <a href="tel:18002738255" ref={this.firstListItemLink} className="vads-u-padding-x--0p5">
+                Call <strong>1-800-273-8255 and press 1</strong>
+              </a>
+            </li>
+            <li className={listItemClasses}>
+              <FontAwesomeIcon icon={faMobileAlt} className={iconClasses} />
+              <a href="sms:838255" className="vads-u-padding-x--0p5">Text <strong>838255</strong></a>
+            </li>
+            <li className={listItemClasses}>
+              <FontAwesomeIcon icon={faComments} className={iconClasses} />
+              <a href="https://www.veteranscrisisline.net/ChatTermsOfService.aspx?account=Veterans%20Chat"
+                className="vads-u-padding-x--0p5"
+              >
+                Start a confidential chat
+              </a>
+            </li>
+            <li className={listItemClasses}>
+              <FontAwesomeIcon icon={faDeaf} className={iconClasses} />
+              <a href="tel:18007994889" className="vads-u-padding-x--0p5">
+                Call TTY if you have hearing loss <strong>1-800-799-4889</strong>
+              </a>
+            </li>
+          </ul>
 
-          <div className="va-overlay-body va-crisis-panel-body">
-            <h3 className="va-crisis-panel-title">
-              <img className="vcl-logo" src={vclLogo} alt="Veterans Crisis Line logo" 
-                  aria-label="Veterans Crisis Line logo" />
-              <span>We're here anytime, day or night - 24/7</span>
-            </h3>
-            <p>
-              If you are a Veteran in crisis or concerned about one, connect with our caring,
-              qualified responders for confidential help. Many of them are Veterans themselves.
-            </p>
-            <ul className="va-crisis-panel-list">
-              <li>
-                <FontAwesomeIcon icon={faPhone} className="va-crisis-panel-icon" />
-                <a href="tel:18002738255" ref={this.firstListItemLink}>
-                  Call <strong>1-800-273-8255 and press 1</strong>
-                </a>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faMobileAlt} className="va-crisis-panel-icon" />
-                <a href="sms:838255">Text <strong>838255</strong></a>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faComments} className="va-crisis-panel-icon" />
-                <a href="https://www.veteranscrisisline.net/ChatTermsOfService.aspx?account=Veterans%20Chat">
-                  Start a confidential chat
-                </a>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faDeaf} className="va-crisis-panel-icon" />
-                <a href="tel:18007994889">
-                  Call TTY if you have hearing loss <strong>1-800-799-4889</strong>
-                </a>
-              </li>
-            </ul>
-
-            <p>
-              Get more resources at &nbsp;
-              <a href="https://www.veteranscrisisline.net/">VeteransCrisisLine.net</a>.
-            </p>
-          </div>
+          <p>
+            Get more resources at &nbsp;
+            <a href="https://www.veteranscrisisline.net/">VeteransCrisisLine.net</a>.
+          </p>
         </div>
       </div>
     );
