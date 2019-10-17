17b7e2fc6e56e96c9b242c963f5ab658ef7367f4
Josh Salisbury <josh@adhocteam.us>
2019-10-17 14:12:47 -0500

    Updates based on feedback from PR

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,34 +2,30 @@ import classNames from 'classnames';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../assets/hero-logo.png';
+import logo from '../assets/hero-logo.svg';
 
 export default function Hero() {
-  const heroClasses = classNames(
-    'vads-u-background-color--primary-darkest',
-    'vads-u-padding-y--5',
-    'vads-u-padding-x--0',
-  );
-
-  const containerClasses = classNames(
-    "vads-u-display--flex",
-    "small-desktop-screen:vads-u-flex-direction--row",
-    "vads-u-flex-direction--column-reverse",
-    "vads-l-grid-container",
-    "vads-u-margin-x--auto",
-  );
-
   return (
-    <section role="region" aria-label="Page Hero" className={heroClasses}>
-      <div className={containerClasses}>
-        <div className={classNames("", "va-api-margin-y--auto")}>
+    <section role="region" aria-label="Page Hero" className={classNames(
+      'vads-u-background-color--primary-darkest',
+      'vads-u-padding-y--5',
+      'vads-u-padding-x--0',
+    )}>
+      <div className={classNames(
+        "vads-u-display--flex",
+        "small-desktop-screen:vads-u-flex-direction--row",
+        "vads-u-flex-direction--column-reverse",
+        "vads-l-grid-container",
+        "vads-u-margin-x--auto",
+      )}>
+        <div className={classNames("va-api-margin-y--auto")}>
           <div className="vads-u-max-width--100">
-            <h1 className="vads-u-color--white">A Veteran-centered API platform for securely accessing VA data.</h1>
+            <h1 className={classNames("vads-u-color--white", "vads-u-font-size--h2", "small-desktop-screen:vads-u-font-size--h1")}>A Veteran-centered API platform for securely accessing VA data.</h1>
             <Link id="hero-request-key" to="/apply" className="usa-button vads-u-width--full medium-screen:vads-u-width--auto">Request an API Key</Link>
           </div>
         </div>
-        <div className={classNames("", "vads-u-margin-x--auto", "va-api-margin-y--auto")}>
-          <img src={logo} alt="Department of Veterans Affairs" />
+        <div className={classNames("small-desktop-screen:vads-u-width--auto", "va-api-home__image", "vads-u-margin-x--auto", "va-api-margin-y--auto")}>
+          <img src={logo} alt="" role="presentation" />
         </div>
       </div>
     </section>
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -4,62 +4,31 @@ import { Link } from 'react-router-dom';
 
 import { Flag } from 'flag';
 import apiDefinitions, { apiCategoryOrder } from '../apiDefs/data/categories';
-import padlockImg from '../assets/homepage-padlock.png';
-import apiImg from '../assets/homepage-reliable-api.png';
+import padlockImg from '../assets/homepage-padlock.svg';
+import apiImg from '../assets/homepage-reliable-api.svg';
 import CardLink from '../components/CardLink';
 import Hero from '../components/Hero';
 import './Home.scss';
 
 const leftColumnClasses = classNames('medium-screen:vads-l-col--4', 'small-screen:vads-l-col--6', 'va-api-margin-y--auto');
-const rightColumnClasses = classNames('medium-screen:vads-l-col--8', 'small-screen:vads-l-col--6', 'va-api-margin-y--auto');
+const rightColumnClasses = classNames('medium-screen:vads-l-col--8', 'small-screen:vads-u-padding-left--2', 'small-screen:vads-l-col--6', 'va-api-margin-y--auto');
 const flexContainer = classNames('vads-l-grid-container', 'vads-u-margin-top--6', 'vads-u-margin-x--auto');
+const imageClasses = classNames('medium-screen:vads-u-width--auto', 'va-api-home__image');
 
-function ApiPlatform() {
+
+function HomeSection({ariaLabel, imageSrc, title, children} : {ariaLabel: string, imageSrc: string, title: string, children: React.ReactNode}) {
   return (
-    <section role="region" aria-label="An API platform">
+    <section role="region" aria-label={ariaLabel}>
       <div className={flexContainer}>
         <div className="vads-l-row">
           <div className={leftColumnClasses}>
-            <img src={padlockImg} alt="padlock-image" />
+            <img className={imageClasses} src={imageSrc} alt="" role="presentation" />
           </div>
           <div className={rightColumnClasses}>
             <h2>
-              A secure API platform to service Veterans.
+              {title}
             </h2>
-            <div>
-              Lighthouse is an API platform that gives developers secure access to the VA data they need to build helpful tools and services for Veterans.
-              Streamlined and accessible, we designed a modern API development experience to easily build services that provide Veterans with consistent
-              and reliable access to their data.
-              &nbsp;<Link to="/documention">Read our documentation</Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-function ReliableApi() {
-  return (
-    <section role="region" aria-label="reliable API">
-      <div className={flexContainer}>
-        <div className="vads-l-row">
-          <div className={leftColumnClasses}>
-            <img src={apiImg} alt="api image" />
-          </div>
-          <div className={rightColumnClasses}>
-            <h2>
-              A modern, reliable API library.
-            </h2>
-            <div>
-              Our library consists of APIs for Benefits, Health, Facilities, and Veteran Verification information. These APIs allow approved individuals
-              and organizations to access VA information systems at no cost.
-            </div>
-            <div className="vads-u-display--none medium-screen:vads-u-display--block">
-              <br />
-              API Consumers can immediately request API key access to sandbox data to explore how they can use VA APIs to create tools that serve Veterans.
-              Leveraging RESTful API, Open API and OAuth 2.0 standards, our APIs enable Veterans and service providers to achieve the following goals:
-            </div>
+              {children}
           </div>
         </div>
       </div>
@@ -121,8 +90,25 @@ class Home extends React.Component {
       <div className="home">
         <Hero />
         <VeteransNotice />
-        <ApiPlatform />
-        <ReliableApi />
+        <HomeSection ariaLabel="An API platform" imageSrc={padlockImg} title="A secure API platform to service Veterans.">
+          <div>
+            Lighthouse is an API platform that gives developers secure access to the VA data they need to build helpful tools and services for Veterans.
+            Streamlined and accessible, we designed a modern API development experience to easily build services that provide Veterans with consistent
+            and reliable access to their data.
+            &nbsp;<Link to="/documention">Read our documentation</Link>
+          </div>
+        </HomeSection>
+        <HomeSection ariaLabel="reliable API" imageSrc={apiImg} title="A modern, reliable API library.">
+          <div>
+            Our library consists of APIs for Benefits, Health, Facilities, and Veteran Verification information. These APIs allow approved individuals
+            and organizations to access VA information systems at no cost.
+          </div>
+          <div className="vads-u-display--none medium-screen:vads-u-display--block">
+            <br />
+            API Consumers can immediately request API key access to sandbox data to explore how they can use VA APIs to create tools that serve Veterans.
+            Leveraging RESTful API, Open API and OAuth 2.0 standards, our APIs enable Veterans and service providers to achieve the following goals:
+          </div>
+        </HomeSection>
         <ApiList />
       </div>
     );
