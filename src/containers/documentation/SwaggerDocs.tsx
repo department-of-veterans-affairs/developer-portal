a9e9c8f759db670246823d38ed241ce534bf1744
Will Huang <52384927+whuang85@users.noreply.github.com>
2019-11-20 14:38:54 -0600

    correct logic when no metadata

--- src/containers/documentation/SwaggerDocs.tsx
+++ src/containers/documentation/SwaggerDocs.tsx
@@ -139,7 +139,7 @@ export default class SwaggerDocs extends React.Component<ISwaggerDocsProps, ISwa
 
   private updateURLHash() {
     const versionInfo = this.getVersionInfo(this.props.apiVersion, this.state.metadata);
-    const version = versionInfo ? versionInfo.version : this.props.apiVersion;
+    const version = versionInfo ? versionInfo.version : null;
     const hash = (!version || (versionInfo && versionInfo.status === SwaggerDocs.currentVersionStatus)) ? SwaggerDocs.currentHash : `v${version}`;
     history.push(`${history.location.pathname}#${hash}`);
   }
