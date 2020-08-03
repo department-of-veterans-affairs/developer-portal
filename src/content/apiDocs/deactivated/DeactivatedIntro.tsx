879daf56f8634c826ca785327a6442b0ab08b0a4
Steve Novosel <steve.novosel@oddball.io>
2020-08-03 16:41:54 -0400

    banner created for content

--- src/content/apiDocs/deactivated/DeactivatedIntro.tsx
+++ src/content/apiDocs/deactivated/DeactivatedIntro.tsx
@@ -1,16 +1,29 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import * as React from 'react';
 
 import './DeactivatedIntro.scss';
 
 const DeactivatedIntro = () => {
+
   return (
-    <div>
-      <div />
-      <div>
-        <FontAwesomeIcon icon={faInfoCircle} />
-        <p>This is a repository for deactivated APIs and related documentation and release notes.</p>
+    <div className={classNames(
+      'vads-u-display--flex',
+    )}>
+      <div className={classNames(
+        'deactivated-intro-flag',
+        'vads-u-background-color--primary-alt-dark',
+      )} />
+      <div className={classNames(
+        'vads-u-flex--1',
+        'vads-u-display--flex',
+        'vads-u-align-items--center',
+        'vads-u-padding-x--3',
+        'vads-u-background-color--gray-light-alt',
+      )}>
+        <FontAwesomeIcon icon={faInfoCircle} className={classNames('vads-u-margin-right--2')} />
+        <p className={classNames('vads-u-font-weight--bold')}>This is a repository for deactivated APIs and related documentation and release notes.</p>
       </div>
     </div>
   );
