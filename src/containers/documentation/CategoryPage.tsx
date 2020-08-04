736391464ba8096568a7680f6d899c21f96580b0
Steve Novosel <steve.novosel@oddball.io>
2020-08-04 18:08:35 -0400

    add test for deactivation info

--- src/containers/documentation/CategoryPage.tsx
+++ src/containers/documentation/CategoryPage.tsx
@@ -47,7 +47,7 @@ export default class CategoryPage extends React.Component<RouteComponentProps<IA
         );
       });
 
-      const authCard = apis.some(api => !!api.oAuth) && categoryName !== 'Benefits API'  ? <AuthorizationCard categoryKey={apiCategoryKey} /> : null;
+      const authCard = apis.some(api => !!api.oAuth) && categoryName !== 'Benefits API' && apiCategoryKey !== 'deactivated' ? <AuthorizationCard categoryKey={apiCategoryKey} /> : null;
 
       cardSection = (
         <div role="navigation" aria-labelledby={headerId}>
