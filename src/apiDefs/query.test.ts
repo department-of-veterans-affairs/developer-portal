f2333d993324afe52b9bc1a9bae0a1046a407d3e
stephennovosel <steve.novosel@oddball.io>
2020-08-11 14:53:31 -0400

    revert query.test.ts

--- src/apiDefs/query.test.ts
+++ src/apiDefs/query.test.ts
@@ -73,6 +73,7 @@ describe('query module', () => {
       expect(verificationApi).not.toBeNull();
       expect(verificationApi!.apis.length).toBeGreaterThanOrEqual(3);
       expect(verificationApi!.apis.map(api => !!api.oAuth).filter(m => m).length).toEqual(2);
+
     });
 
     it('returns null for an API that does not exist', () => {
