e61a46561fad72294c82119bfd55b03d0eb7dd53
Giles Wells <giles@mediagearhead.com>
2020-09-04 11:11:03 -0400

    Fixed unit test

--- src/containers/apply/ApplyHeader.tsx
+++ src/containers/apply/ApplyHeader.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import PageHeader from 'src/components/PageHeader';
+import PageHeader from '../../components/PageHeader';
 
 export default function ApplyHeader() {
   return (
