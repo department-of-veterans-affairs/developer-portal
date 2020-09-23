bd079d0949bb74d72256bb6396401bd808a8ae8b
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-09-23 14:19:53 -0400

    Use React Hooks - ServersContainer

--- src/containers/documentation/swaggerPlugins/ServersContainer.jsx
+++ src/containers/documentation/swaggerPlugins/ServersContainer.jsx
@@ -1,41 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class ServersContainer extends React.Component {
+export default function ServersContainer(props) {
+  const {specSelectors, oas3Selectors, oas3Actions, getComponent} = props;
 
-  static propTypes = {
-    getComponent: PropTypes.func.isRequired,
-    oas3Actions: PropTypes.object.isRequired,
-    oas3Selectors: PropTypes.object.isRequired,
-    specSelectors: PropTypes.object.isRequired
-  }
+  const servers = specSelectors.servers();
 
-  render () {
-    const {specSelectors, oas3Selectors, oas3Actions, getComponent} = this.props;
+  const Col = getComponent("Col");
+  const Servers = getComponent("Servers");
 
-    const servers = specSelectors.servers();
+  return (
+    <div>
+      {servers && servers.size ? (
+        <div className="global-server-container">
+          <Col className="servers wrapper" mobile={12}>
+            <h3 className="servers-title">Server</h3>
+            <Servers
+              servers={servers}
+              currentServer={oas3Selectors.selectedServer()}
+              setSelectedServer={oas3Actions.setSelectedServer}
+              setServerVariableValue={oas3Actions.setServerVariableValue}
+              getServerVariable={oas3Selectors.serverVariableValue}
+              getEffectiveServerValue={oas3Selectors.serverEffectiveValue}
+            />
+          </Col>
+        </div>
+      ) : null}
+    </div>
+  );
+}
 
-    const Col = getComponent("Col");
-    const Servers = getComponent("Servers");
-
-    return (
-      <div>
-        {servers && servers.size ? (
-          <div className="global-server-container">
-            <Col className="servers wrapper" mobile={12}>
-              <h3 className="servers-title">Server</h3>
-              <Servers
-                servers={servers}
-                currentServer={oas3Selectors.selectedServer()}
-                setSelectedServer={oas3Actions.setSelectedServer}
-                setServerVariableValue={oas3Actions.setServerVariableValue}
-                getServerVariable={oas3Selectors.serverVariableValue}
-                getEffectiveServerValue={oas3Selectors.serverEffectiveValue}
-              />
-            </Col>
-          </div>
-        ) : null}
-      </div>
-    );
-  }
+ServersContainer.propTypes = {
+  getComponent: PropTypes.func.isRequired,
+  oas3Actions: PropTypes.object.isRequired,
+  oas3Selectors: PropTypes.object.isRequired,
+  specSelectors: PropTypes.object.isRequired
 }
