ff2da9e1d5b3afeec6c97ee6366f3b57aaeb828b
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-09-25 13:25:04 -0400

    Use ReactHooks for VeteransCrisisLine
    To try and clean up the component I made a custom hook for controlling
    the modal. I didn't see any other modals in the code, or at least not
    obvious modals, to have a reason to pull the custom Effect out into a
    different file. Maybe we could create an effects/ folder in our src code
    to store any custom effects we create for easy reuse?

--- src/components/crisisLine/VeteransCrisisLine.tsx
+++ src/components/crisisLine/VeteransCrisisLine.tsx
@@ -9,70 +9,70 @@ import './VeteransCrisisLine.scss';
 
 import rightArrow from '../../assets/arrow-right-white.svg';
 
-interface IVeteransCrisisLineState {
-  modalVisible: boolean;
-}
+function useModalController() {
+  const [modalVisible, setModalVisible] = React.useState(false);
 
-export default class VeteransCrisisLine extends React.Component<{}, IVeteransCrisisLineState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { modalVisible: false };
-  }
-
-  public componentDidMount() {
-    document.addEventListener("keydown", this.handleEscape, false);
-  }
-
-  public componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleEscape, false);
-  }
-
-  public render() {
-    return (
-      <div className={classNames(
-        'va-crisis-line',
-        'vads-u-margin-right--0',
-        'medium-screen:vads-u-margin-right--4',
-      )}>
-        <button 
-          data-show="#crisis-line-modal"
-          onClick={() => this.setState({ modalVisible: true })}
-          className={classNames(
-            'va-crisis-line-button',
-            'va-api-crisis-line-button',
-            'vads-u-color--white',
-            'vads-u-display--flex',
-            'vads-u-justify-content--center',
-            'vads-u-padding-right--0',
-            'vads-u-width--full',
-            'medium-screen:vads-u-width--auto',
-        )}>
-          <span className={classNames('vads-u-display--flex', 'vads-u-align-items--center')}>
-            <span className={classNames('va-api-crisis-line-container', 'vads-u-margin-right--1')}>
-              <span className={classNames('vcl', 'va-api-vcl-logo-white')} />
-            </span>
-            <span className="vads-u-margin-right--1">
-              Talk to the&nbsp;<strong>Veterans Crisis Line</strong>&nbsp;now
-            </span>
-            <img src={rightArrow} 
-              className={classNames('va-api-right-arrow', 'vads-u-margin-right--1')} 
-              alt="" role="presentation" 
-            />
-          </span>
-        </button>
-        <Modal id="crisis-line-modal" visible={this.state.modalVisible}
-          onClose={() => this.setState({ modalVisible: false })}
-          initialFocusSelector="li > a"
-        >
-          <VeteransCrisisLinePanel />
-        </Modal>
-      </div>
-    );
-  }
-
-  private handleEscape = (event: KeyboardEvent) => {
-    if (event.key === 'Escape' && this.state.modalVisible) {
-      this.setState({ modalVisible: false });
+  const handleEscape = (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && modalVisible) {
+      setModalVisible(false);
     }
-  }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleEscape, false);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape, false);
+    };
+  }, []);
+
+  return {modalVisible, setModalVisible};
 }
+
+const VeteransCrisisLine = () => {
+
+  const {modalVisible, setModalVisible} = useModalController();
+
+  return (
+    <div className={classNames(
+      'va-crisis-line',
+      'vads-u-margin-right--0',
+      'medium-screen:vads-u-margin-right--4',
+    )}>
+      <button 
+        data-show="#crisis-line-modal"
+        onClick={() => setModalVisible(true)}
+        className={classNames(
+          'va-crisis-line-button',
+          'va-api-crisis-line-button',
+          'vads-u-color--white',
+          'vads-u-display--flex',
+          'vads-u-justify-content--center',
+          'vads-u-padding-right--0',
+          'vads-u-width--full',
+          'medium-screen:vads-u-width--auto',
+      )}>
+        <span className={classNames('vads-u-display--flex', 'vads-u-align-items--center')}>
+          <span className={classNames('va-api-crisis-line-container', 'vads-u-margin-right--1')}>
+            <span className={classNames('vcl', 'va-api-vcl-logo-white')} />
+          </span>
+          <span className="vads-u-margin-right--1">
+            Talk to the&nbsp;<strong>Veterans Crisis Line</strong>&nbsp;now
+          </span>
+          <img src={rightArrow} 
+            className={classNames('va-api-right-arrow', 'vads-u-margin-right--1')} 
+            alt="" role="presentation" 
+          />
+        </span>
+      </button>
+      <Modal id="crisis-line-modal" visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        initialFocusSelector="li > a"
+      >
+        <VeteransCrisisLinePanel />
+      </Modal>
+    </div>
+  );
+};
+
+export default VeteransCrisisLine;
