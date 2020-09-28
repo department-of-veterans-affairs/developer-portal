f3a8573192009756f84f2df0e01117eedd85d30e
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-09-28 13:34:45 -0400

    Revert changing OperationTag to use React Hooks
    OperationTag is from SwaggerUI that we're maintaining. In order to keep
    this similar to the original, for future updating, we're leaving these
    in the original implementation without react hooks.

--- src/containers/documentation/swaggerPlugins/OperationTag.jsx
+++ src/containers/documentation/swaggerPlugins/OperationTag.jsx
@@ -7,98 +7,102 @@ import { sanitizeUrl } from "@braintree/sanitize-url";
 
 const createDeepLinkPath = (str) => typeof str === "string" || str instanceof String ? str.trim().replace(/\s/g, "_") : "";
 
-export default function OperationTag(props = {
-  tag: '',
-  tagObj: Im.fromJS({}),
-}) {
+export default class OperationTag extends React.Component {
 
-  const {
-    children,
-    getConfigs,
-    getComponent,
-    layoutSelectors,
-    layoutActions,
-    tag,
-    tagObj
-  } = props;
+  static defaultProps = {
+    tag: "",
+    tagObj: Im.fromJS({})
+  };
 
-  const {
-    deepLinking,
-    docExpansion
-  } = getConfigs();
+  static propTypes = {
+    tag: PropTypes.string.isRequired,
+    tagObj: ImPropTypes.map.isRequired,
 
-  const isDeepLinkingEnabled = deepLinking && deepLinking !== "false";
+    layoutActions: PropTypes.object.isRequired,
+    layoutSelectors: PropTypes.object.isRequired,
 
-  const Collapse = getComponent("Collapse");
-  const Markdown = getComponent("Markdown");
-  const DeepLink = getComponent("DeepLink");
+    getComponent: PropTypes.func.isRequired,
+    getConfigs: PropTypes.func.isRequired
+  };
 
-  let tagDescription = tagObj.getIn(["tagDetails", "description"], null);
-  let tagExternalDocsDescription = tagObj.getIn(["tagDetails", "externalDocs", "description"]);
-  let tagExternalDocsUrl = tagObj.getIn(["tagDetails", "externalDocs", "url"]);
+  render() {
+    const {
+      children,
+      getConfigs,
+      getComponent,
+      layoutSelectors,
+      layoutActions,
+      tag,
+      tagObj
+    } = this.props;
 
-  let isShownKey = ["operations-tag", createDeepLinkPath(tag)];
-  let showTag = layoutSelectors.isShown(isShownKey, docExpansion === "full" || docExpansion === "list");
+    let {
+      deepLinking,
+      docExpansion
+    } = getConfigs();
 
-  return (
-    <div className={showTag ? "opblock-tag-section is-open" : "opblock-tag-section"} >
+    const isDeepLinkingEnabled = deepLinking && deepLinking !== "false";
 
-      <h3
-        onClick={() => layoutActions.show(isShownKey, !showTag)}
-        className={!tagDescription ? "opblock-tag no-desc" : "opblock-tag" }
-        id={isShownKey.join("-")}>
-        <DeepLink
-          enabled={isDeepLinkingEnabled}
-          isShown={showTag}
-          path={tag}
-          text={tag} />
-        { !tagDescription ? <small></small> :
-          <small>
-              <Markdown source={tagDescription} />
-            </small>
-          }
+    const Collapse = getComponent("Collapse");
+    const Markdown = getComponent("Markdown");
+    const DeepLink = getComponent("DeepLink");
 
-          <div>
-            { !tagExternalDocsDescription ? null :
-              <small>
-                  { tagExternalDocsDescription }
-                    { tagExternalDocsUrl ? ": " : null }
-                    { tagExternalDocsUrl ?
-                      <a
-                          href={sanitizeUrl(tagExternalDocsUrl)}
-                          onClick={(e) => e.stopPropagation()}
-                          target={"_blank"}
-                          >{tagExternalDocsUrl}</a> : null
-                        }
-                </small>
-              }
-          </div>
+    let tagDescription = tagObj.getIn(["tagDetails", "description"], null);
+    let tagExternalDocsDescription = tagObj.getIn(["tagDetails", "externalDocs", "description"]);
+    let tagExternalDocsUrl = tagObj.getIn(["tagDetails", "externalDocs", "url"]);
 
-          <button
-            className="expand-operation"
-            title={showTag ? "Collapse operation": "Expand operation"}
-            onClick={() => layoutActions.show(isShownKey, !showTag)}>
+    let isShownKey = ["operations-tag", createDeepLinkPath(tag)];
+    let showTag = layoutSelectors.isShown(isShownKey, docExpansion === "full" || docExpansion === "list");
 
-            <svg className="arrow" width="20" height="20">
-              <use href={showTag ? "#large-arrow-down" : "#large-arrow"} xlinkHref={showTag ? "#large-arrow-down" : "#large-arrow"} />
-            </svg>
-          </button>
-      </h3>
+    return (
+      <div className={showTag ? "opblock-tag-section is-open" : "opblock-tag-section"} >
 
-      <Collapse isOpened={showTag}>
-        {children}
-      </Collapse>
-    </div>
-  );
+        <h3
+          onClick={() => layoutActions.show(isShownKey, !showTag)}
+          className={!tagDescription ? "opblock-tag no-desc" : "opblock-tag" }
+          id={isShownKey.join("-")}>
+          <DeepLink
+            enabled={isDeepLinkingEnabled}
+            isShown={showTag}
+            path={tag}
+            text={tag} />
+          { !tagDescription ? <small></small> :
+            <small>
+                <Markdown source={tagDescription} />
+              </small>
+            }
+
+            <div>
+              { !tagExternalDocsDescription ? null :
+                <small>
+                    { tagExternalDocsDescription }
+                      { tagExternalDocsUrl ? ": " : null }
+                      { tagExternalDocsUrl ?
+                        <a
+                            href={sanitizeUrl(tagExternalDocsUrl)}
+                            onClick={(e) => e.stopPropagation()}
+                            target={"_blank"}
+                            >{tagExternalDocsUrl}</a> : null
+                          }
+                  </small>
+                }
+            </div>
+
+            <button
+              className="expand-operation"
+              title={showTag ? "Collapse operation": "Expand operation"}
+              onClick={() => layoutActions.show(isShownKey, !showTag)}>
+
+              <svg className="arrow" width="20" height="20">
+                <use href={showTag ? "#large-arrow-down" : "#large-arrow"} xlinkHref={showTag ? "#large-arrow-down" : "#large-arrow"} />
+              </svg>
+            </button>
+        </h3>
+
+        <Collapse isOpened={showTag}>
+          {children}
+        </Collapse>
+      </div>
+    );
+  }
 }
-
-OperationTag.propTypes = {
-  tag: PropTypes.string.isRequired,
-  tagObj: ImPropTypes.map.isRequired,
-
-  layoutActions: PropTypes.object.isRequired,
-  layoutSelectors: PropTypes.object.isRequired,
-
-  getComponent: PropTypes.func.isRequired,
-  getConfigs: PropTypes.func.isRequired
-};
