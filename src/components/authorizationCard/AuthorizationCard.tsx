aabaf7eee67240548d8622a24a031734fc629769
Johanna Del Pino <johanna@adhocteam.us>
2020-09-30 13:44:35 -0400

    Fixed interface naming convention.

--- src/components/authorizationCard/AuthorizationCard.tsx
+++ src/components/authorizationCard/AuthorizationCard.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 
 import CardLink from '../CardLink';
 
-const AuthorizationCardProps = {
+const AuthorizationCardPropTypes = {
   categoryKey: PropTypes.string.isRequired,
 };
 
-type IAuthorizationCardProps = PropTypes.InferProps<typeof AuthorizationCardProps>;
+type AuthorizationCardProps = PropTypes.InferProps<typeof AuthorizationCardPropTypes>;
 
-const AuthorizationCard = (props: IAuthorizationCardProps): JSX.Element => (
+const AuthorizationCard = (props: AuthorizationCardProps): JSX.Element => (
   <CardLink name="Authorization" url={`/explore/${props.categoryKey}/docs/authorization`}>
     Use the OpenID Connect standard to allow Veterans to authorize third-party application to access
     data on their behalf.
   </CardLink>
 );
 
-AuthorizationCard.propTypes = AuthorizationCardProps;
+AuthorizationCard.propTypes = AuthorizationCardPropTypes;
 
 export { AuthorizationCard };
