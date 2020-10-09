d510e641179acd9cca6a989e14c7c47fbbe0d43e
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-10-09 10:03:07 -0400

    Add tests to the 'usePrevious' hook

--- src/hooks/previous/Previous.tsx
+++ src/hooks/previous/Previous.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 // Use generics so that we can use this for any type
 const usePrevious = <T extends unknown>(props?: T): T | null => {
-  const theRef = React.useRef<T | null>(props ?? null);
+  const theRef = React.useRef<T | null>(null);
 
   React.useEffect(() => {
     theRef.current = props ?? null;
