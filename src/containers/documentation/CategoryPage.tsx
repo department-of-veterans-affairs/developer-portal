0da098511bb147819d925043b1df5b3c54e4e20a
TS000 <tylersnell@me.com>
2020-10-15 10:32:27 -0600

    fixed flag path

--- src/containers/documentation/CategoryPage.tsx
+++ src/containers/documentation/CategoryPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useParams } from 'react-router';
-import { Flag } from 'flag';
+import { Flag } from '../../../src/flags';
 import { getApiDefinitions } from '../../apiDefs/query';
 import { APIDescription } from '../../apiDefs/schema';
 import { AuthorizationCard, OnlyTags } from '../../components';
