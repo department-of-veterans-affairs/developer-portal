f5c23419ea6c2f4349c986e013377398bf1e8e7f
TS000 <tylersnell@me.com>
2020-11-02 10:46:28 -0700

    current changes from meeting with Johanna

--- src/utils/fetchWrap.ts
+++ src/utils/fetchWrap.ts
@@ -1,25 +1,32 @@
 import * as Sentry from '@sentry/browser';
-
-const sentryErrorLogger = (error: string, errorID: string): void => {
+import { v4 as uuidv4 } from 'uuid';
+const sentryErrorLogger = (error: string, errorID: string, endpointUrl:string): void => {
+  const pageName =  location.pathname;
+  console.info(`sentry log:${error} - x-request-id: ${errorID}- endpointUrl:${endpointUrl} - pageName: ${pageName}`);
   Sentry.withScope(scope => {
     scope.setTag('Error ID', errorID);
+    scope.setTag('Endpoint Url', endpointUrl);
+    scope.setTag('Page Name', pageName);
     Sentry.captureException(error);
   });
 };
-
-const fetchMiddleware = (response: Response, errorID: string): void => {
-  if ((!response.ok && response.status !== 300) || 400 || 500) {
-    throw TypeError(`Front End Error ID: ${errorID}`);
+const fetchMiddleware = (response: Response, errorDetails?: string): void => {
+  console.log(response);
+  if ((!response.ok && response.status !== 300)) {
+    response.json().then(content=>{console.log('the json details get resolved');console.log(content);});
+    throw TypeError(`HTTP Non Network Error: ${errorDetails||''}`);
   }
 };
-
-export const fetchWrap = async (request: Request, errorID: string): Promise<Response> => {
+export const makeRequest = async (request: Request): Promise<Response> => {
+  const requestId:string = uuidv4() as string;
+  /* common headers */
+  request.headers.append('X-Request-ID',requestId);
   try {
     const response = await fetch(request);
-    fetchMiddleware(response, errorID);
+    fetchMiddleware(response, requestId);
     return response;
   } catch (error) {
-    sentryErrorLogger(error, errorID);
+    sentryErrorLogger(error, requestId, request.url);
     throw new Error(error);
   }
 };
