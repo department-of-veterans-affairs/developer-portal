fad31e086927a4cf9e8844557925f32be765264f
TS000 <tylersnell@me.com>
2020-11-02 11:08:33 -0700

    JSON only wrapper

--- src/utils/fetchWrap.ts
+++ src/utils/fetchWrap.ts
@@ -1,8 +1,11 @@
 import * as Sentry from '@sentry/browser';
 import { v4 as uuidv4 } from 'uuid';
-const sentryErrorLogger = (error: string, errorID: string, endpointUrl:string): void => {
-  const pageName =  location.pathname;
-  console.info(`sentry log:${error} - x-request-id: ${errorID}- endpointUrl:${endpointUrl} - pageName: ${pageName}`);
+
+const sentryErrorLogger = (error: string, errorID: string, endpointUrl: string): void => {
+  const pageName = location.pathname;
+  console.info(
+    `sentry log:${error} - x-request-id: ${errorID}- endpointUrl:${endpointUrl} - pageName: ${pageName}`,
+  );
   Sentry.withScope(scope => {
     scope.setTag('Error ID', errorID);
     scope.setTag('Endpoint Url', endpointUrl);
@@ -11,19 +14,18 @@ const sentryErrorLogger = (error: string, errorID: string, endpointUrl:string): 
   });
 };
 const fetchMiddleware = (response: Response, errorDetails?: string): void => {
-  console.log(response);
-  if ((!response.ok && response.status !== 300)) {
-    response.json().then(content=>{console.log('the json details get resolved');console.log(content);});
-    throw TypeError(`HTTP Non Network Error: ${errorDetails||''}`);
+  if (!response.ok && response.status !== 300) {
+    throw TypeError(`HTTP Non Network Error: ${errorDetails || ''}`);
   }
 };
 export const makeRequest = async (request: Request): Promise<Response> => {
-  const requestId:string = uuidv4() as string;
+  const requestId: string = uuidv4() as string;
   /* common headers */
-  request.headers.append('X-Request-ID',requestId);
+  request.headers.append('X-Request-ID', requestId);
   try {
     const response = await fetch(request);
-    fetchMiddleware(response, requestId);
+    const json = await response.json();
+    fetchMiddleware(json, requestId);
     return response;
   } catch (error) {
     sentryErrorLogger(error, requestId, request.url);
