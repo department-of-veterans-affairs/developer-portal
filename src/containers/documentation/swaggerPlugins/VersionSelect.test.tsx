f99dc1894d85254c09949108b884cd1e3574c7b1
Alexander James Valencia <alexanderjamesvalencia@AlexanderValencia-Macbook.local>
2020-11-09 15:58:43 -0600

    feat: clean up tests for VersionSelect

--- src/containers/documentation/swaggerPlugins/VersionSelect.test.tsx
+++ src/containers/documentation/swaggerPlugins/VersionSelect.test.tsx
@@ -1,11 +1,11 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import Im from 'immutable';
 import 'jest';
 import { SwaggerMapValues } from 'swagger-ui';
 import { VersionMetadata } from 'src/types';
-import * as React from 'react';
+import React from 'react';
 import { System } from './types';
 import VersionSelect from './VersionSelect';
 
@@ -26,67 +26,87 @@ const versionMetadata: VersionMetadata[] = [
   },
 ];
 
-const updateVersionMock = jest.fn<void, []>();
+const updateVersionMock = jest.fn<never, []>();
 
-const mockGetSystem = (metadata: VersionMetadata[] | null = null): jest.Mock<System, []> =>
+const mockGetSystem = (
+  metadata: VersionMetadata[] | null,
+  apiVersion: string = '',
+): jest.Mock<System, []> =>
   jest.fn(
     (): System => ({
       fn: {
-        curlify: () => '',
+        curlify: (): string => '',
       },
-      spec: () => {
-        const map = Im.Map<string, SwaggerMapValues>([['test', {}]]);
-        return map;
-      },
+      spec: (): Im.Map<string, SwaggerMapValues> => Im.Map([['test', {}]]),
       versionActions: {
-        setApiVersion: () => null,
-        setVersionMetadata: () => null,
+        setApiVersion: (): null => null,
+        setVersionMetadata: (): null => null,
         updateVersion: updateVersionMock,
       },
       versionSelectors: {
-        apiVersion: () => '',
-        majorVersion: () => '',
-        versionMetadata: () => metadata,
+        apiVersion: (): string => apiVersion,
+        majorVersion: (): string => '',
+        versionMetadata: (): VersionMetadata[] | null => metadata,
       },
     }),
   );
 
 describe('VersionSelect', () => {
   beforeEach(() => {
+    const getSystem = mockGetSystem(versionMetadata);
+    render(<VersionSelect getSystem={getSystem} />);
+
     updateVersionMock.mockReset();
   });
 
   it('renders successfully', () => {
-    const getSystem = mockGetSystem();
-    render(<VersionSelect getSystem={getSystem} />);
-
-    expect(screen.getByRole('button')).toHaveTextContent('Select');
+    expect(screen.getByRole('combobox', { name: 'Version Selection' })).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
-  it('has select options when metadata is not null', () => {
-    const getSystem = mockGetSystem(versionMetadata);
-    const { getByDisplayValue } = render(<VersionSelect getSystem={getSystem} />);
+  it('has the options passed by the version metadata', () => {
+    const options = screen.getAllByRole('option') as HTMLOptionElement[];
 
-    const select = getByDisplayValue('1.0.0 - Current Version');
-    expect(select).toBeInTheDocument();
+    expect(options[0].value).toBe('1.0.0');
+    expect(options[1].value).toBe('0.0.1');
+  });
 
-    fireEvent.change(select, { target: { value: '0.0.1' } });
+  it('changes value to the selected option', () => {
+    const select = screen.getByRole('combobox', { name: 'Version Selection' }) as HTMLSelectElement;
 
-    expect(getByDisplayValue('0.0.1 - Previous Version (Internal Only)')).toBeInTheDocument();
+    expect(select.value).toBe('1.0.0');
+
+    userEvent.selectOptions(select, '0.0.1');
+
+    expect(select.value).toBe('0.0.1');
+  });
+
+  it('does not fire updateVersion action if the button is not clicked', () => {
+    userEvent.selectOptions(screen.getByRole('combobox', { name: 'Version Selection' }), '0.0.1');
+
+    expect(updateVersionMock).toHaveBeenCalledTimes(0);
   });
 
   describe('when the button is clicked', (): void => {
-    it('fires Swagger UI updateVersion action', (): void => {
-      const getSystem = mockGetSystem(versionMetadata);
-      render(<VersionSelect getSystem={getSystem} />);
+    /* eslint-disable max-nested-callbacks */
+    it('fires updateVersion action', (): void => {
+      const select = screen.getByRole('combobox', { name: 'Version Selection' });
 
-      const select = screen.getByDisplayValue('1.0.0 - Current Version');
-      expect(select).toBeInTheDocument();
-
-      fireEvent.change(select, { target: { value: '0.0.1' } });
+      userEvent.selectOptions(select, '0.0.1');
       userEvent.click(screen.getByRole('button'));
 
       expect(updateVersionMock).toHaveBeenCalledWith('0.0.1');
     });
+    /* eslint-enable max-nested-callbacks */
+  });
+});
+
+describe('VersionSelect with initial version', () => {
+  it('renders version from Redux store as intiial version', () => {
+    const getSystem = mockGetSystem(versionMetadata, '0.0.1');
+    render(<VersionSelect getSystem={getSystem} />);
+
+    const select = screen.getByRole('combobox', { name: 'Version Selection' }) as HTMLSelectElement;
+    expect(select.value).toBe('0.0.1');
   });
 });
