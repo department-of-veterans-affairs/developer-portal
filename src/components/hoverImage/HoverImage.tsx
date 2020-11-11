b10c6d414243909c60a8e39ef2972a40a4895bab
TS000 <tylersnell@me.com>
2020-11-11 08:12:52 -0700

    state for imageAlt and setProps combo function

--- src/components/hoverImage/HoverImage.tsx
+++ src/components/hoverImage/HoverImage.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
 
 interface HoverImageProps {
+  imageAlt: string;
   imagePath: string;
   hoverImagePath: string;
 }
 
 const HoverImage = (props: HoverImageProps): JSX.Element => {
+  const [alt, setAlt] = React.useState(props.imageAlt);
   const [src, setSrc] = React.useState(props.imagePath);
+
+  const setProps = (): void => {
+    setAlt(props.imageAlt);
+    setSrc(props.hoverImagePath);
+  };
 
   return (
     <img
-      alt=""
+      alt={alt}
       role="presentation"
-      src={src}
-      onMouseEnter={(): void => setSrc(props.hoverImagePath)}
+      onMouseEnter={(): void => setProps()}
       onMouseLeave={(): void => setSrc(props.imagePath)}
+      src={src}
     />
   );
 };
