92741ce0708e014948c7026286a117cf3a0c5ed6
TS000 <tylersnell@me.com>
2020-12-09 08:32:39 -0700

    fixed extra space and used screen

--- src/components/hero/Hero.test.tsx
+++ src/components/hero/Hero.test.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import 'jest';
 import '@testing-library/jest-dom/extend-expect';
-import {  render } from '@testing-library/react';
+import {  render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as logoImage from '../../__mocks__/fakeImage';
 import { Hero } from './Hero';
 
 describe('Hero', () => {
   it('checks image source', () => {
-    const { queryByRole } = render(<Router><Hero  /></Router>);
-    const heroImage = queryByRole('presentation');
+    render(<Router><Hero /></Router>);
+    const heroImage = screen.getByRole('presentation');
 
     expect(heroImage).toBeInTheDocument();
     expect(heroImage).toHaveAttribute('src', (logoImage.default));
