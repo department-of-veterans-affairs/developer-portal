15209b12e60049eb885bd974bd164ba0f978472b
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2021-01-21 13:27:43 -0500

    Upgrade react-dev-utils to 11.0.1
    This upgrades react-dev-utils to 11.0.1.
    
    Note that e2e and visual regression tests are not running properly with
    this change. A later commit will be made to address this.
    
    API-4498

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -101,7 +101,8 @@ module.exports = function(proxy, allowedHost) {
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware());
+      const servedPath = '.';
+      app.use(noopServiceWorkerMiddleware(servedPath));
     },
   };
 };
