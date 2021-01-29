47a9f591dfef56ff824f795f7d223c4968b514e5
Christine <yost90@gmail.com>
2021-01-29 09:26:28 -0700

    Update footer to have a border

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,12 +7,13 @@ import './Footer.scss';
 import { SUPPORT_CONTACT_PATH, TERMS_OF_SERVICE_PATH } from '../../types/constants/paths';
 import logo from '../../assets/lighthouseVaLogo.png';
 
-const yellowDivStyles = classNames('vads-u-background-color--gold', 'va-api-footer-yellow');
 const footerStyles = classNames(
   'vads-u-background-color--primary-darkest',
   'vads-u-color--white',
   'vads-u-width--full',
   'vads-u-display--flex',
+  'vads-u-border-top--5px',
+  'vads-u-border-color--gold',
   'vads-u-flex-direction--column',
   'vads-u-padding-x--2',
   'medium-screen:vads-u-flex-direction--row',
@@ -44,40 +45,37 @@ const listItemStyles = classNames(
 );
 
 const Footer: React.FunctionComponent = (): JSX.Element => (
-  <footer role="contentinfo">
-    <div className={yellowDivStyles} />
-    <div className={footerStyles}>
-      <a href="https://www.va.gov" className={vaLinkStyles}>
-        <img src={logo} className="va-api-footer-logo" alt="Department of Veterans Affairs" />
-      </a>
-      <ul className={footerListStyles}>
-        <li className={listItemStyles}>
-          <Link to={SUPPORT_CONTACT_PATH} className={footerLinkStyles}>
-            Contact Us
-          </Link>
-        </li>
-        <li className={listItemStyles}>
-          <Link to={TERMS_OF_SERVICE_PATH} className={footerLinkStyles}>
-            Terms of Service
-          </Link>
-        </li>
-        <li className={listItemStyles}>
-          <a href="https://www.section508.va.gov/" className={footerLinkStyles}>
-            Accessibility
-          </a>
-        </li>
-        <li className={listItemStyles}>
-          <a href="https://www.va.gov/webpolicylinks.asp" className={footerLinkStyles}>
-            Web Policies
-          </a>
-        </li>
-        <li className={listItemStyles}>
-          <a href="https://www.va.gov/privacy/" className={footerLinkStyles}>
-            Privacy
-          </a>
-        </li>
-      </ul>
-    </div>
+  <footer role="contentinfo" className={footerStyles}>
+    <a href="https://www.va.gov" className={vaLinkStyles}>
+      <img src={logo} className="va-api-footer-logo" alt="Department of Veterans Affairs" />
+    </a>
+    <ul className={footerListStyles}>
+      <li className={listItemStyles}>
+        <Link to={SUPPORT_CONTACT_PATH} className={footerLinkStyles}>
+          Contact Us
+        </Link>
+      </li>
+      <li className={listItemStyles}>
+        <Link to={TERMS_OF_SERVICE_PATH} className={footerLinkStyles}>
+          Terms of Service
+        </Link>
+      </li>
+      <li className={listItemStyles}>
+        <a href="https://www.section508.va.gov/" className={footerLinkStyles}>
+          Accessibility
+        </a>
+      </li>
+      <li className={listItemStyles}>
+        <a href="https://www.va.gov/webpolicylinks.asp" className={footerLinkStyles}>
+          Web Policies
+        </a>
+      </li>
+      <li className={listItemStyles}>
+        <a href="https://www.va.gov/privacy/" className={footerLinkStyles}>
+          Privacy
+        </a>
+      </li>
+    </ul>
   </footer>
 );
 
