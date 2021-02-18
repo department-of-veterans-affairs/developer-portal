8fe26bca12111c3f7ad6b07efa9db88e170abaaf
Johanna Del Pino <johanna@adhocteam.us>
2021-02-18 16:37:33 -0500

    Testing lint-stage husky integration.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import 'highlight.js/styles/atom-one-dark-reasonable.css';
 import './styles/atom-one-dark-reasonable-overrides.scss';
 import './styles/base.scss';
 
+// this is a test
+
 declare const window: { VetsGov?: Record<string, unknown> };
 
 /**
