a37413c958f188aa08a81f733db711516b33ca9c
Mike Lumetta <mike.lumetta@adhocteam.us>
2021-02-22 15:02:51 -0500

    Revert "Fix heading order on Contact Us page"
    This reverts commit b8d220288e81876a895615f07967fd486b98a2f3.

--- src/containers/support/ContactUsForm/components/ConsumerFormFields.tsx
+++ src/containers/support/ContactUsForm/components/ConsumerFormFields.tsx
@@ -3,18 +3,16 @@ import React, { FC } from 'react';
 import { FormField } from '../../../../components';
 
 const ConsumerFormFields: FC = () => (
-  <fieldset className="vads-u-margin-top--4">
-    <legend className={classNames('vads-u-font-size--lg', 'vads-u-margin-bottom--0')}>
-      Description
-    </legend>
+  <>
+    <h3 className={classNames('vads-u-margin-top--4', 'vads-u-margin-bottom--0')}>Description</h3>
     <FormField
       label="Describe your question or issue in as much detail as you can. If your question is about an error, include steps you took to get it and any error messaging you received."
       name="description"
       as="textarea"
       required
-      className="vads-u-padding-top--3"
+      className="vads-u-padding-top--4"
     />
-  </fieldset>
+  </>
 );
 
 export default ConsumerFormFields;
