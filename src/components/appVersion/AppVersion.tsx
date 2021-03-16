8c2e8e56388db2c2b08a837e0185bfd6379a4aab
TS000 <tylersnell@me.com>
2021-03-16 12:19:07 -0600

    updated where the hash is pulled from

--- src/components/appVersion/AppVersion.tsx
+++ src/components/appVersion/AppVersion.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './AppVersion.scss';
 
-const currentAppVersion = process.env.REACT_APP_COMMIT_HASH ?? 'undefined';
+const currentAppVersion = process.env.COMMIT_HASH ?? 'undefined';
 
 const AppVersion = (): JSX.Element => (
   <p className="app-commit">{currentAppVersion}</p>
