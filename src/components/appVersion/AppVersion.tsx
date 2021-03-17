b8138a7b9ad109bc0d5093d361e007e10318f96d
TS000 <tylersnell@me.com>
2021-03-17 08:58:34 -0600

    broke test

--- src/components/appVersion/AppVersion.tsx
+++ src/components/appVersion/AppVersion.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import './AppVersion.scss';
 
-const currentAppVersion = process.env.REACT_APP_COMMIT_HASH ?? 'undefined';
+// const currentAppVersion = process.env.REACT_APP_COMMIT_HASH ?? 'undefined';
 const testCommit = process.env.COMMIT_HASH ?? 'undefined';
 
 const AppVersion = (): JSX.Element => (
   <>
-    <p className="app-commit">REACT_APP_COMMIT_HASH: {currentAppVersion}</p>
-    <p className="app-commit">COMMIT_HASH: {testCommit}</p>
+    {/* <p className="app-commit">REACT_APP_COMMIT_HASH: {currentAppVersion}</p> */}
+    <p className="app-commit">{testCommit}</p>
   </>
 );
 
