b15044613feb3b31da68c161ca36fa8de59b7e1f
TS000 <tylersnell@me.com>
2021-03-17 12:03:52 -0600

    move to ID over class since single item

--- src/components/appVersion/AppVersion.tsx
+++ src/components/appVersion/AppVersion.tsx
@@ -4,7 +4,7 @@ import './AppVersion.scss';
 const commitHash = process.env.REACT_APP_COMMIT_HASH ?? 'undefined';
 
 const AppVersion = (): JSX.Element => (
-  <p className="app-commit">{commitHash}</p>
+  <div id="app-commit">Commit Hash: {commitHash}</div>
 );
 
 export { AppVersion };
