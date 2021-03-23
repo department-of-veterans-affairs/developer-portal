ac86d5275c12bb9681c61c5ffc774f5d89ba3e1a
John Borden <john.borden@adhocteam.us>
2021-03-23 09:39:18 -0700

    PR feedback

--- src/containers/apply/OAuthAppInfo.tsx
+++ src/containers/apply/OAuthAppInfo.tsx
@@ -6,14 +6,14 @@ const OAuthAppInfo: React.FunctionComponent = (): JSX.Element => (
   <>
     <div className="vads-u-margin-top--4">
       Please specify whether your app can securely hide a client secret. Apps that can hide a
-      secret will use the&nbsp;
+      secret will use the{' '}
       <a
         href="https://www.oauth.com/oauth2-servers/server-side-apps/authorization-code/"
         target="_blank"
         rel="noreferrer"
       >
         authorization code flow
-      </a>, and apps that cannot will use the&nbsp;
+      </a>, and apps that cannot will use the{' '}
       <a href="https://www.oauth.com/oauth2-servers/pkce/" target="_blank" rel="noreferrer">
         PKCE flow
       </a>.
