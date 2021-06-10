a5f5bf335a94b970cff69e41c9022d83ffc423b4
Nick Sprinkle <ndsprinkle@gmail.com>
2021-06-10 13:00:37 -0400

    Update PageHeader and CategoryPage for specific API pages

--- src/components/pageHeader/PageHeader.tsx
+++ src/components/pageHeader/PageHeader.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
+import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 import { PAGE_HEADER_AND_HALO_ID, PAGE_HEADER_ID } from '../../types/constants';
 import './PageHeader.scss';
 
@@ -9,6 +10,12 @@ interface PageHeaderProps {
   description?: string;
   halo?: string;
   header: string;
+  veteranRedirect?: {
+    message: string;
+    linkUrl: string;
+    linkText: string;
+  };
+  apiCategoryKey?: string;
 }
 
 const PageHeader = (props: PageHeaderProps): JSX.Element => (
@@ -23,6 +30,12 @@ const PageHeader = (props: PageHeaderProps): JSX.Element => (
     >
       {props.header}
     </h1>
+    {props.veteranRedirect && (
+      <AlertBox status="info" key={props.apiCategoryKey} className={classNames('vads-u-margin-bottom--2', 'vads-u-padding-y--1')}>
+        {props.veteranRedirect.message}&nbsp;
+        <a href={props.veteranRedirect.linkUrl}>{props.veteranRedirect.linkText}</a>.
+      </AlertBox>
+    )}
     {props.description && (
       <h2 className={classNames('vads-u-font-size--lg')}>{props.description}</h2>
     )}
--- src/containers/documentation/CategoryPage.tsx
+++ src/containers/documentation/CategoryPage.tsx
@@ -1,14 +1,14 @@
-import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 import * as React from 'react';
 import Helmet from 'react-helmet';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Flag, useFlag } from '../../flags';
 import { getApiDefinitions } from '../../apiDefs/query';
 import { APIDescription } from '../../apiDefs/schema';
 import { AuthorizationCard, CardLink, OnlyTags, PageHeader } from '../../components';
 import { defaultFlexContainer } from '../../styles/vadsUtils';
 import { APINameParam } from '../../types';
-import { FLAG_AUTH_DOCS_V2, FLAG_HOSTED_APIS, PAGE_HEADER_ID } from '../../types/constants';
+import { FLAG_AUTH_DOCS_V2, FLAG_HOSTED_APIS, PAGE_HEADER_ID, FLAG_CONSUMER_DOCS } from '../../types/constants';
 
 const CategoryPage = (): JSX.Element => {
   const authDocsV2 = useFlag([FLAG_AUTH_DOCS_V2]);
@@ -61,17 +61,23 @@ const CategoryPage = (): JSX.Element => {
       <Helmet>
         <title>{categoryName}</title>
       </Helmet>
-      <PageHeader header={categoryName} />
+      <PageHeader
+        header={categoryName}
+        veteranRedirect={veteranRedirect}
+        apiCategoryKey={apiCategoryKey}
+      />
       {intro({})}
+      <div className="vads-u-width--full">
+        {overview({})}
+        <Flag name={[FLAG_CONSUMER_DOCS]}>
+          <p>
+            <Link to="/consumer-docs">
+              Read the consumer onboarding guide for getting production access
+            </Link>.
+          </p>
+        </Flag>
+      </div>
       {cardSection}
-      <div className="vads-u-width--full">{overview({})}</div>
-      <hr />
-      {veteranRedirect && (
-        <AlertBox status="info" key={apiCategoryKey}>
-          {veteranRedirect.message}&nbsp;
-          <a href={veteranRedirect.linkUrl}>{veteranRedirect.linkText}</a>.
-        </AlertBox>
-      )}
     </div>
   );
 };
