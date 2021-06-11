bf20331f15e7f24ff429e06961b6ed90190964e6
Mike Lumetta <mike.lumetta@adhocteam.us>
2021-06-11 12:34:07 -0400

    Add process list/subway map to consumer onboarding overview

--- src/containers/consumerOnboarding/OnboardingOverview.test.tsx
+++ src/containers/consumerOnboarding/OnboardingOverview.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { getAllByRole, render, screen } from '@testing-library/react';
 import React from 'react';
 import OnboardingOverview from './OnboardingOverview';
 
@@ -7,8 +7,49 @@ describe('OnboardingOverview', () => {
     render(<OnboardingOverview />);
   });
 
-  it('renders the heading', () => {
-    const heading = screen.getByRole('heading', { name: 'API Consumer Onboarding' });
+  it('renders the main heading', () => {
+    const heading = screen.getByRole('heading', { level: 1, name: 'API Consumer Onboarding' });
     expect(heading).toBeInTheDocument();
+  });
+
+  it.each([
+    'Onboarding steps',
+    'Onboarding timeline',
+    'About us',
+  ])('renders the "%s" heading', (headingText: string) => {
+    const heading = screen.getByRole('heading', { level: 2, name: headingText });
+    expect(heading).toBeInTheDocument();
+  });
+
+  describe('subway map/process list', () => {
+    it('renders the subway map', () => {
+      const list = screen.getByRole('list', { name: 'Onboarding steps' });
+      expect(list).toBeInTheDocument();
+    });
+
+    describe('steps', () => {
+      let steps: HTMLElement[];
+      beforeEach(() => {
+        const list = screen.getByRole('list', { name: 'Onboarding steps' });
+        steps = getAllByRole(list, 'listitem');
+      });
+
+      it('has 4 steps', () => {
+        expect(steps).toHaveLength(4);
+      });
+
+      it.each([
+        ['Start developing', 1],
+        ['Request production access', 2],
+        ['Prepare for and complete a demo', 3],
+        ['Receive production access', 4],
+      ])('includes the "%s" step (step %d)', (stepName: string, stepNumber: number) => {
+        expect(steps[stepNumber - 1]).toBeInTheDocument();
+
+        const firstChild = steps[stepNumber - 1].children[0];
+        expect(firstChild.tagName).toBe('STRONG');
+        expect(firstChild).toHaveTextContent(stepName);
+      });
+    });
   });
 });
--- src/containers/consumerOnboarding/OnboardingOverview.tsx
+++ src/containers/consumerOnboarding/OnboardingOverview.tsx
@@ -2,7 +2,47 @@ import React from 'react';
 import { PageHeader } from '../../components';
 
 const ConsumerOnboardingOverview = (): JSX.Element => (
-  <PageHeader header="API Consumer Onboarding" />
+  <>
+    <PageHeader header="API Consumer Onboarding" />
+    <p>
+      Our API consumer onboarding process ensures the quality and security of applications integrating
+      with our APIs and data. It may seem complex, but don’t worry—we’re in this together and are here
+      to answer your questions each step of the way.
+    </p>
+    <h2 id="onboarding-steps">Onboarding steps</h2>
+    <ol className="process" aria-labelledby="onboarding-steps">
+      <li className="process-step list-one" aria-labelledby="start-developing">
+        <strong id="start-developing">Start developing</strong>
+        <p>
+          Access to our sandbox environment is automatic when you request an API key.
+        </p>
+      </li>
+      <li className="process-step list-two" aria-labelledby="request-prod-access">
+        <strong id="request-prod-access">Request production access</strong>
+        <p>
+          Getting production access can take less than a week to more than a month, depending on the API.
+        </p>
+        <p>
+          Learn what’s needed on the production access form.
+        </p>
+      </li>
+      <li className="process-step list-three" aria-labelledby="demo">
+        <strong id="demo">Prepare for and complete a demo</strong>
+        <p>
+          We’ll review your production access request. Any changes we require must be made before your
+          demo. Open data APIs don’t require a demo.
+        </p>
+      </li>
+      <li className="process-step list-four" aria-labelledby="receive-prod-access">
+        <strong id="receive-prod-access">Receive production access</strong>
+        <p>
+          After a successful demo, you’ll get production access. Learn more about working with our APIs.
+        </p>
+      </li>
+    </ol>
+    <h2>Onboarding timeline</h2>
+    <h2>About us</h2>
+  </>
 );
 
 export default ConsumerOnboardingOverview;
