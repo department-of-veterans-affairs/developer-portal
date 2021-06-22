fc925e7a23ac737754400ed55300109a24847a14
ediiotero <eddie.otero@oddball.io>
2021-06-22 11:27:02 -0400

    add role alert to error message span

--- src/containers/apply/SelectedApis.tsx
+++ src/containers/apply/SelectedApis.tsx
@@ -70,34 +70,34 @@ const apiInfo = [
 ];
 
 const SelectedAPIs = (): JSX.Element => {
-  const { errors, touched } = useFormikContext();
+  const { errors } = useFormikContext();
   const checkboxName = 'apis';
-  const shouldDisplayErrors = !!errors[checkboxName] && !!touched[checkboxName];
+  const shouldDisplayErrors = !!errors[checkboxName];
   const containerClass = shouldDisplayErrors ? 'usa-input-error' : '';
   const labelClass = shouldDisplayErrors ? 'usa-input-error-label' : '';
   const validationClass = shouldDisplayErrors ? 'usa-input-error-message' : '';
   const selectAPIClass = shouldDisplayErrors ? 'vads-u-font-weight--bold' : 'vads-u-font-weight--normal';
 
   return (
-    <fieldset aria-label="API Select checkboxes" className={classNames(containerClass, 'apply-api-select')}>
+    <fieldset aria-labelledby="select-checkbox-api" className={classNames(containerClass, 'apply-api-select')}>
       <div className="vads-u-margin-top--1 apply-checkbox-labels">
         <legend id="select-checkbox-api" className={classNames(selectAPIClass, 'vads-u-font-size--base', labelClass)}>
           Select the APIs you want to access. <span className="apply-required">&#40;*Required&#41;</span>
         </legend>
-        <span id="api-checkbox-error" className={validationClass} aria-live="polite">
+        <span id="api-checkbox-error" className={validationClass} role="alert">
           <ErrorMessage name="apis" />
         </span>
         <p>You can always request access to more APIs later.</p>
         <fieldset
           className="vads-u-margin-top--2"
-          aria-labelledby="select-checkbox-api select-checkbox-standard"
+          aria-labelledby="select-checkbox-standard"
         >
           <legend id="select-checkbox-standard" className="vads-u-font-size--lg">Standard APIs:</legend>
           <ApiCheckboxList apiCheckboxes={apiInfo} />
         </fieldset>
         <fieldset
           className="vads-u-margin-top--2"
-          aria-labelledby="select-checkbox-api select-checkbox-oauth"
+          aria-labelledby="select-checkbox-oauth"
         >
           <legend id="select-checkbox-oauth" className="vads-u-font-size--lg">OAuth APIs:</legend>
           <ApiCheckboxList apiCheckboxes={oauthInfo} />
