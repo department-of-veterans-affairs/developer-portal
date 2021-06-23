e8f21a94ccf2ffa04269ed6e6777c06433a44e14
keithosayande <keith.osayande@adhocteam.us>
2021-06-23 16:00:05 -0400

    Updated CardLink.test.tsx to work with new CardLink component.

--- src/components/cardLink/CardLink.test.tsx
+++ src/components/cardLink/CardLink.test.tsx
@@ -8,48 +8,51 @@ describe('CardLink', () => {
   it('renders the name', () => {
     render(
       <Router>
-        <CardLink name="Special API" url="/special">
+        <CardLink name="Special API" url="/special" linkText="View Special API">
           Use this to manage something!
         </CardLink>
       </Router>,
     );
 
-    const link = screen.getByRole('link', { name: /^Special API/ });
+    const link = screen.getByRole('link', { name: 'View Special API' });
     expect(link).toBeInTheDocument();
-    expect(link.firstElementChild).toHaveTextContent('Special API');
+    expect(link.parentElement).toHaveTextContent('Special API');
   });
 
   it('renders the description', () => {
     render(
       <Router>
-        <CardLink name="Special API" url="/special">
+        <CardLink name="Special API" url="/special" linkText="View Special API">
           Use this to manage something!
         </CardLink>
       </Router>,
     );
 
-    const link = screen.getByRole('link', { name: /^Special API/ });
+    const link = screen.getByRole('link', { name: 'View Special API' });
     expect(link).toBeInTheDocument();
-    expect(link.firstElementChild?.nextElementSibling).toHaveTextContent(
-      'Use this to manage something!'
-    );
+    expect(link.parentElement?.children[2]).toHaveTextContent('Use this to manage something!');
   });
 
   it('renders the subhead between the name and description', () => {
     render(
       <Router>
-        <CardLink name="Special API" subhead={<div>Test subhead</div>} url="/special">
+        <CardLink
+          name="Special API"
+          subhead={<div>Test subhead</div>}
+          url="/special"
+          linkText="View Special API"
+        >
           Use this to manage something!
         </CardLink>
       </Router>,
     );
 
-    const link = screen.getByRole('link', { name: /^Special API/ });
+    const link = screen.getByRole('link', { name: 'View Special API' });
     expect(link).toBeInTheDocument();
-    expect(link.childElementCount).toBe(3);
+    expect(link.parentElement?.childElementCount).toBe(5);
 
-    expect(link.children[0]).toHaveTextContent('Special API');
-    expect(link.children[1]).toHaveTextContent('Test subhead');
-    expect(link.children[2]).toHaveTextContent('Use this to manage something!');
+    expect(link.parentElement?.children[1]).toHaveTextContent('Special API');
+    expect(link.parentElement?.children[2]).toHaveTextContent('Test subhead');
+    expect(link.parentElement?.children[3]).toHaveTextContent('Use this to manage something!');
   });
 });
