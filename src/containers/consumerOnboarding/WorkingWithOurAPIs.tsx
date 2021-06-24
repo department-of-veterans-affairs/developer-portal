00228c6a5894a93e13832c2f4e6f1a78468fe70a
keithosayande <keith.osayande@adhocteam.us>
2021-06-24 09:35:32 -0400

    initial commit.

---
+++ src/containers/consumerOnboarding/WorkingWithOurAPIs.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { PageHeader } from '../../components';
+
+const WorkingWithOurAPIs = (): JSX.Element => (
+  <div>
+    <PageHeader header="Working with Lighthouse APIs" />
+    <p>
+      Integrating with Lighthouse APIs can help your app’s users get faster, more reliable access to
+      the data they need. This page tells you more about what you can expect while working with
+      Lighthouse APIs, including information about rate limiting, feature requests, status updates,
+      and more.
+    </p>
+  </div>
+);
+
+export default WorkingWithOurAPIs;
