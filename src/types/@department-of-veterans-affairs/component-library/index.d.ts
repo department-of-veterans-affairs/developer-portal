26df24a9730d8d9b5b055cd2e2d49bee453575c4
Giles Wells <giles@mediagearhead.com>
2021-08-04 17:28:50 -0400

    Update VaAccordion module declarations

--- src/types/@department-of-veterans-affairs/component-library/index.d.ts
+++ src/types/@department-of-veterans-affairs/component-library/index.d.ts
@@ -1,10 +1,22 @@
 declare module '@department-of-veterans-affairs/component-library/AlertBox';
-declare module '@department-of-veterans-affairs/component-library/CollapsiblePanel';
 declare module '@department-of-veterans-affairs/component-library/Modal';
+
+interface VaAccordionProps {
+  bordered?: boolean;
+  children: JSX.Element | JSX.Element[];
+  disableAnalytics?: boolean;
+  openSingle?: boolean;
+}
+interface VaAccordionItemProps {
+  header: string;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+  open?: boolean;
+  subheader?: string;
+}
 
 declare namespace JSX {
   interface IntrinsicElements {
-    'va-accordion': unknown;
+    'va-accordion': VaAccordionProps;
     'va-accordion-item': unknown;
   }
 }
