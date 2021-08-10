52dbec68fba0fb27883bf903d862bc9ef8031c89
Giles Wells <giles@mediagearhead.com>
2021-08-10 15:34:59 -0400

    remove console log eslint-disable

--- src/components/groupedAccordions/GroupedAccordions.test.tsx
+++ src/components/groupedAccordions/GroupedAccordions.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { getByRole, queryByText, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import 'jest';
