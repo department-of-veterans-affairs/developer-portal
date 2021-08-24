84815878bd719dabb1e79d089d74a6f3115016cd
Mike Lumetta <mike.lumetta@adhocteam.us>
2021-08-24 10:34:50 -0400

    Remove formatting changes from the merge from this branch and PR

--- src/types/constants/index.ts
+++ src/types/constants/index.ts
@@ -6,8 +6,9 @@ export const CURRENT_VERSION_IDENTIFIER = 'current';
 export const DEFAULT_OAUTH_API_SELECTION = 'claims';
 export const OPEN_API_SPEC_HOST: string = process.env.REACT_APP_VETSGOV_SWAGGER_API ?? '';
 
-const BACKEND_BASE_URL = `${process.env.REACT_APP_DEVELOPER_PORTAL_SELF_SERVICE_URL ?? ''
-  }/internal/developer-portal/public`;
+const BACKEND_BASE_URL = `${
+  process.env.REACT_APP_DEVELOPER_PORTAL_SELF_SERVICE_URL ?? ''
+}/internal/developer-portal/public`;
 export const APPLY_URL = `${BACKEND_BASE_URL}/developer_application`;
 export const PRODUCTION_ACCESS_URL = `${BACKEND_BASE_URL}/production_request`;
 export const CONTACT_US_URL = `${BACKEND_BASE_URL}/contact-us`;
