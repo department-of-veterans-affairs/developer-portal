892095bdd3da1f8735c824593f643f6dd68b84c6
Mike Lumetta <mike.lumetta@adhocteam.us>
2021-09-01 12:45:53 -0400

    Polishing code cleanliness for changes in CheckboxRadioField

--- src/components/formField/CheckboxRadioField/CheckboxRadioField.tsx
+++ src/components/formField/CheckboxRadioField/CheckboxRadioField.tsx
@@ -30,28 +30,34 @@ const CheckboxRadioField: FC<CheckboxRadioFieldProps> = ({
 
   const idReadyName = toHtmlId(name);
   const idReadyValue = toHtmlId(value ?? '');
-  const shouldDisplayErrors = showError && !!getIn(errors, name) && !!getIn(touched, name);
+  const applyErrorStyles = showError && !!getIn(errors, name) && !!getIn(touched, name);
   const errorId = `${idReadyName}FormFieldError`;
   const fieldId = `${idReadyName}FormField${idReadyValue}`;
-  const validationClass = shouldDisplayErrors ? 'usa-input-error-message' : '';
-  const containerClass = shouldDisplayErrors ? 'usa-input-error' : '';
 
   return (
-    <div className={classNames(containerClass, className)}>
+    <div
+      className={classNames({
+        'usa-input-error': applyErrorStyles,
+      }, className)}
+    >
       <Field
         id={fieldId}
         name={name}
         type={type}
         value={value}
         innerRef={innerRef}
-        aria-invalid={shouldDisplayErrors}
+        aria-invalid={applyErrorStyles}
         {...props}
       />
       <label htmlFor={fieldId} className={radioClass}>
         {label}
       </label>
       {showError && (
-        <span id={errorId} className={validationClass} role="alert">
+        <span
+          id={errorId}
+          className={classNames({ 'usa-input-error-message': applyErrorStyles })}
+          role="alert"
+        >
           <ErrorMessage name={name} />
         </span>
       )}
