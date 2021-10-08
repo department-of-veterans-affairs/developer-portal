801a9e4c898461183c7465f74f5160c678bde8c7
Lee DeBoom <lee.deboom@oddball.io>
2021-10-08 13:54:12 -0500

    API-10443

--- src/containers/documentation/ClientCredentialsGrant/ClientCredentialsGrantDocs.tsx
+++ src/containers/documentation/ClientCredentialsGrant/ClientCredentialsGrantDocs.tsx
@@ -1,10 +1,18 @@
+import { History } from 'history';
 import * as React from 'react';
 import Helmet from 'react-helmet';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router';
 import {
   PageHeader,
   APISelector
 } from '../../../components';
+import {
+  resetOAuthApiSelection,
+  ResetOAuthAPISelection,
+  setOAuthApiSelection,
+  SetOAuthAPISelection,
+} from '../../../actions';
 import { GettingStarted } from '../../../components/oauthDocs/ccg/GettingStarted';
 import { AuthCodeFlowContent } from '../../../components/oauthDocs/ccg/AuthCodeFlowContent';
 import { TestUsers } from '../../../components/oauthDocs/ccg/TestUsers';
@@ -12,6 +20,8 @@ import { getAllOauthApis } from '../../../apiDefs/query';
 import { isApiDeactivated } from '../../../apiDefs/deprecated';
 import { APIDescription } from '../../../apiDefs/schema';
 import { RootState } from '../../../types';
+import { usePrevious } from '../../../hooks';
+import { DEFAULT_OAUTH_CCG_API_SELECTION } from '../../../types/constants';
 
 interface ClientCredentialsFlowContentProps {
   options: APIDescription[];
@@ -19,12 +29,71 @@ interface ClientCredentialsFlowContentProps {
   apiDef?: APIDescription | null;
 }
 
+const setSearchParam = (
+  history: History,
+  queryString: string,
+  api: string,
+  initialLoad: boolean,
+): void => {
+  const params = new URLSearchParams(queryString);
+  if (params.get('api') !== api) {
+    params.set('api', api);
+    if (initialLoad) {
+      history.replace(`${history.location.pathname}?${params.toString()}${history.location.hash}`);
+    } else {
+      history.push(`${history.location.pathname}?${params.toString()}`);
+    }
+  }
+};
+
+const setInitialApi = (
+  history: History,
+  searchQuery: string,
+  dispatch: React.Dispatch<ResetOAuthAPISelection | SetOAuthAPISelection>,
+): void => {
+  const params = new URLSearchParams(searchQuery || undefined);
+  const apiQuery = params.get('api');
+  const availableApis = getAllOauthApis().filter((item: APIDescription) => !isApiDeactivated(item));
+  const isAnApi = availableApis.some((item: APIDescription) => item.urlFragment === apiQuery);
+  const api = apiQuery && isAnApi ? apiQuery.toLowerCase() : DEFAULT_OAUTH_CCG_API_SELECTION;
+  dispatch(setOAuthApiSelection(api));
+  setSearchParam(history, searchQuery, api, true);
+};
+
 const ClientCredentialsGrantDocs = (): JSX.Element => {
+  const history = useHistory();
+  const location = useLocation();
+  const dispatch: React.Dispatch<ResetOAuthAPISelection | SetOAuthAPISelection> = useDispatch();
+  const initializing = React.useRef(true);
+  const api = useSelector((state: RootState) => state.oAuthApiSelection.selectedOAuthApi);
+  const prevApi = usePrevious(api);
+  const selectedOAuthApi = useSelector(
+    (state: RootState) => state.oAuthApiSelection.selectedOAuthApi,
+  );
+
   const options = getAllOauthApis().filter((item: APIDescription) => !isApiDeactivated(item) && 
                                                                      item.oAuthTypes && 
                                                                      item.oAuthTypes.includes("ClientCredentialsGrant"));
-  const selectedOAuthApi = useSelector(
-    (state: RootState) => state.oAuthApiSelection.selectedOAuthApi,
+
+  React.useEffect(() => {
+    if (initializing.current) {
+      // Do this on first load
+      initializing.current = false;
+      setInitialApi(history, location.search, dispatch);
+    } else {
+      // Do this on all subsequent re-renders
+      setSearchParam(history, location.search, api, false);
+    }
+  }, [dispatch, location, history, prevApi, api]);
+
+  /**
+   * CLEAR REDUX STATE ON UNMOUNT
+   */
+  React.useEffect(
+    () => (): void => {
+      dispatch(resetOAuthApiSelection());
+    },
+    [], // eslint-disable-line react-hooks/exhaustive-deps
   );
 
   return (
