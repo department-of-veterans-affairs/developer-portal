9a17aabc01c887a49378a4a2b4aab7d641c18255
Giles Wells <giles@mediagearhead.com>
2022-02-04 14:56:04 -0500

    Toggle PR draft status to run the PR checks

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -85,8 +85,6 @@ const Header = (): JSX.Element => {
               <div className={defaultFlexContainer(true)}>
                 <NavHashLink to={CONSUMER_SANDBOX_PATH} className={buttonClassnames}>
                   Request an API Key
-                  <br />
-                  test
                 </NavHashLink>
                 <Search />
               </div>
