c6ad83fb4d9677adaaf9e2ffa39d6fe35e97ceee
Giles Wells <giles@mediagearhead.com>
2022-05-09 14:55:33 -0400

    Weird version numbers version

--- src/containers/documentation/swaggerPlugins/VersionSelect.tsx
+++ src/containers/documentation/swaggerPlugins/VersionSelect.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import classNames from 'classnames';
 import * as React from 'react';
 import { VersionMetadata } from '../../../types';
@@ -32,6 +33,12 @@ export default class VersionSelect extends React.Component<VersionSelectProps, V
     return versions?.find(selectCurrentVersion)?.version ?? '';
   }
 
+  public getVersionMetadataByProp(prop: string, version: string): VersionMetadata | undefined {
+    const versions = this.props.getSystem().versionSelectors.versionMetadata();
+    const versionMatch = (versionInfo: VersionMetadata): boolean => versionInfo[prop] === version;
+    return versions?.find(versionMatch);
+  }
+
   public handleSelectChange(version: string): void {
     this.setState({ version });
   }
@@ -43,38 +50,42 @@ export default class VersionSelect extends React.Component<VersionSelectProps, V
   public render(): JSX.Element {
     const buildDisplay = (meta: VersionMetadata): string => {
       const { version, status, internal_only } = meta;
-      return `${version} - ${status} ${internal_only ? '(Internal Only)' : ''}`;
+      if (version.includes('-')) {
+        return `${status} ${internal_only ? '(Internal Only)' : ''}`;
+      } else {
+        return `${version} - ${status} ${internal_only ? '(Internal Only)' : ''}`;
+      }
     };
     const fhirRegex = /\/explore\/health\/docs\/(patient_health|fhir)/;
     const selectorLabel = fhirRegex.test(location.pathname)
       ? 'Select a FHIR specification'
       : 'Select a version';
 
+    let apiStatus;
+    if (this.props.getSystem().versionSelectors.apiVersion() === '') {
+      apiStatus = (
+        this.props.getSystem().versionSelectors.versionMetadata()?.[0] as VersionMetadata
+      ).status;
+    } else {
+      apiStatus = this.getVersionMetadataByProp(
+        'version',
+        this.props.getSystem().versionSelectors.apiVersion(),
+      )?.status;
+    }
+
     return (
-      <div className="api-selector-container">
-        <label htmlFor="version-selector">
-          {selectorLabel}
-          <div
-            className={classNames(
-              'vads-u-display--flex',
-              'vads-u-flex-wrap--wrap',
-              'vads-u-justify-content--flex-start',
-            )}
-          >
+      <>
+        <div className="api-selector-container">
+          <label htmlFor="version-selector">
+            {selectorLabel}
             <div
               className={classNames(
-                'vads-u-display--inline-block',
-                'vads-u-flex--4',
-                'vads-u-margin-right--4',
-                'va-api-u-min-width--200',
+                'vads-u-display--flex',
+                'vads-u-flex-wrap--wrap',
+                'vads-u-justify-content--flex-start',
               )}
             >
-              {/* eslint-disable-next-line jsx-a11y/no-onchange */}
-              <select
-                id="version-selector"
-                aria-label="Version Selection"
-                value={this.state.version}
-                onChange={(e): void => this.handleSelectChange(e.target.value)}
+              <div
                 className={classNames(
                   'vads-u-display--inline-block',
                   'vads-u-flex--4',
@@ -82,26 +93,41 @@ export default class VersionSelect extends React.Component<VersionSelectProps, V
                   'va-api-u-min-width--200',
                 )}
               >
-                {this.props
-                  .getSystem()
-                  .versionSelectors.versionMetadata()
-                  ?.map((versionInfo: VersionMetadata) => (
-                    <option value={versionInfo.version} key={versionInfo.version}>
-                      {buildDisplay(versionInfo)}
-                    </option>
-                  ))}
-              </select>
-              <button
-                onClick={(): void => this.handleButtonClick()}
-                className={classNames('vads-u-flex--1', 'va-api-u-max-width--150')}
-                type="button"
-              >
-                Select
-              </button>
+                {/* eslint-disable-next-line jsx-a11y/no-onchange */}
+                <select
+                  id="version-selector"
+                  aria-label="Version Selection"
+                  value={this.state.version}
+                  onChange={(e): void => this.handleSelectChange(e.target.value)}
+                  className={classNames(
+                    'vads-u-display--inline-block',
+                    'vads-u-flex--4',
+                    'vads-u-margin-right--4',
+                    'va-api-u-min-width--200',
+                  )}
+                >
+                  {this.props
+                    .getSystem()
+                    .versionSelectors.versionMetadata()
+                    ?.map((versionInfo: VersionMetadata) => (
+                      <option value={versionInfo.version} key={versionInfo.version}>
+                        {buildDisplay(versionInfo)}
+                      </option>
+                    ))}
+                </select>
+                <button
+                  onClick={(): void => this.handleButtonClick()}
+                  className={classNames('vads-u-flex--1', 'va-api-u-max-width--150')}
+                  type="button"
+                >
+                  Select
+                </button>
+              </div>
             </div>
-          </div>
-        </label>
-      </div>
+          </label>
+        </div>
+        {fhirRegex.test(location.pathname) && <h2>{apiStatus}</h2>}
+      </>
     );
   }
 }
