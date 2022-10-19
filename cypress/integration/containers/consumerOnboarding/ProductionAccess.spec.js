1ce83bc60aba5cf76dff6660009b7a2549e205b9
Andres Rivera <andres.rivera.toro@gmail.com>
2022-10-19 15:31:30 -0400

    test: update values

--- cypress/integration/containers/consumerOnboarding/ProductionAccess.spec.js
+++ cypress/integration/containers/consumerOnboarding/ProductionAccess.spec.js
@@ -55,7 +55,7 @@ describe('Production Access Form', () => {
   it('Test US-based companies only modal', () => {
     cy.get('#isUSBasedCompanyFormFieldno').click();
     cy.get('#is508CompliantFormFieldyes').click();
-    cy.get('#apisFormFieldappeals').click();
+    cy.get('#apisFormFieldapikeyappeals').click();
     cy.get('#termsOfServiceFormField').click();
     cy.get('button.usa-button[type=submit]').click();
     cy.get('#non-us-based-modal-title').should('be.visible');
@@ -93,7 +93,7 @@ describe('Production Access Form', () => {
     cy.get('#termsOfServiceFormField').click();
     cy.get('#main button[type="submit"]').click();
     cy.get('.usa-input-error').should('have.length', '1');
-    cy.get('#apisFormFieldappeals').click();
+    cy.get('#apisFormFieldapikeyappeals').click();
     cy.get('#main button[type="submit"]').click();
     cy.get('.usa-input-error').should('have.length', '0');
   });
@@ -108,7 +108,7 @@ describe('Production Access Form', () => {
     });
 
     verificationFields(cy);
-    cy.get('#apisFormFieldfacilities').click();
+    cy.get('#apisFormFieldapikeyfacilities').click();
     cy.get('#main button[type="submit"]').click();
 
     cy.focused()
@@ -136,7 +136,7 @@ describe('Production Access Form', () => {
     });
 
     verificationFields(cy);
-    cy.get('#apisFormFieldclinicalHealth').click();
+    cy.get('#apisFormFieldccgclinicalHealth').click();
     cy.get('#main button[type="submit"]').click();
 
     cy.focused()
@@ -169,7 +169,7 @@ describe('Production Access Form', () => {
     });
 
     verificationFields(cy);
-    cy.get('#apisFormFieldclaims').click();
+    cy.get('#apisFormFieldccgclaims').click();
     cy.get('#main button[type="submit"]').click();
 
     cy.focused()
