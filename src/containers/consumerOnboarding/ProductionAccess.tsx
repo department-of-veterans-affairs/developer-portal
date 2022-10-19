e7ea7fd48b5fc97fb65369c43233a72f9b0f663a
Andres Rivera <andres.rivera.toro@gmail.com>
2022-10-19 17:00:31 -0400

    chore: add oauth properties to initial values

--- src/containers/consumerOnboarding/ProductionAccess.tsx
+++ src/containers/consumerOnboarding/ProductionAccess.tsx
@@ -42,6 +42,9 @@ const STEP_HEADING_ID = 'form-step-heading';
 
 export interface Values {
   apis: string[];
+  oAuthApplicationType?: string;
+  oAuthPublicKey?: string;
+  oAuthRedirectURI?: string;
   is508Compliant?: string;
   isUSBasedCompany?: string;
   termsOfService?: boolean;
@@ -106,6 +109,9 @@ const initialValues: Values = {
   monitizedVeteranInformation: '',
   multipleReqSafeguards: '',
   namingConvention: '',
+  oAuthApplicationType: '',
+  oAuthPublicKey: '',
+  oAuthRedirectURI: '',
   organization: '',
   phoneNumber: '',
   piiStorageMethod: '',
