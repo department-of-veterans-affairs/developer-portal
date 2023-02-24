45eaec4f93b2a53e05a0a3371a9e2ec170de1a78
Giles Wells <giles@mediagearhead.com>
2023-02-24 17:26:35 -0500

    Rip out dynamic url -> sitemap logic

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Switch } from 'react-router';
 import { Redirect, Route } from 'react-router-dom';
 
-import { getActiveApiDefinitions, getApiCategoryOrder, getApisLoadedState } from './apiDefs/query';
 import ConsumerOnboardingRoot from './containers/consumerOnboarding/ConsumerOnboardingRoot';
 import DocumentationRoot from './containers/documentation/DocumentationRoot';
 import Home from './containers/Home';
@@ -18,135 +17,79 @@ import {
   CONSUMER_SANDBOX_PATH,
   PUBLISHING_ROUTER_PATHS,
 } from './types/constants/paths';
-import { buildApiDetailRoutes } from './utils/routesHelper';
 import ProductionAccess from './containers/consumerOnboarding/ProductionAccess';
 import ErrorPage404 from './containers/ErrorPage404';
 import TermsOfService from './containers/TermsOfService';
 import IntegrationGuide from './containers/providers/IntegrationGuide';
-import { apiLoadingState } from './types/constants';
 
-export const SiteRoutes: React.FunctionComponent = (): JSX.Element => {
-  const apiDefinitions = getActiveApiDefinitions();
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/index.html" component={Home} />
+export const SiteRoutes: React.FunctionComponent = (): JSX.Element => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/index.html" component={Home} />
 
-      {/* Legacy routes that we want to maintain: */}
-      <Route
-        path="/explore/terms-of-service"
-        render={(): JSX.Element => <Redirect to="/terms-of-service" />}
-      />
-      <Route path="/whats-new" render={(): JSX.Element => <Redirect to="/about/news" />} />
-      <Route path="/news" render={(): JSX.Element => <Redirect to="/about/news" />} />
-      <Route path="/oauth" render={(): JSX.Element => <Redirect to="/explore/authorization" />} />
+    {/* Legacy routes that we want to maintain: */}
+    <Route
+      path="/explore/terms-of-service"
+      render={(): JSX.Element => <Redirect to="/terms-of-service" />}
+    />
+    <Route path="/whats-new" render={(): JSX.Element => <Redirect to="/about/news" />} />
+    <Route path="/news" render={(): JSX.Element => <Redirect to="/about/news" />} />
+    <Route path="/oauth" render={(): JSX.Element => <Redirect to="/explore/authorization" />} />
 
-      {/* Current routes: */}
-      <Route path="/terms-of-service" component={TermsOfService} />
+    {/* Current routes: */}
+    <Route path="/terms-of-service" component={TermsOfService} />
 
-      {/* API Documentation */}
-      <Route exact path="/explore" component={DocumentationRoot} />
-      <Route exact path="/explore/authorization" component={DocumentationRoot} />
-      <Route
-        exact
-        path="/explore/authorization/docs/authorization-code"
-        component={DocumentationRoot}
-      />
-      <Route
-        exact
-        path="/explore/authorization/docs/client-credentials"
-        component={DocumentationRoot}
-      />
-      {buildApiDetailRoutes(apiDefinitions).map(
-        (path: string): JSX.Element => (
-          <Route exact key={path} path={path} component={DocumentationRoot} />
-        ),
-      )}
-      {getApisLoadedState() === apiLoadingState.IN_PROGRESS && (
-        <Route path="/explore/:name" component={DocumentationRoot} />
-      )}
+    {/* API Documentation */}
+    <Route exact path="/explore" component={DocumentationRoot} />
+    <Route exact path="/explore/authorization" component={DocumentationRoot} />
+    <Route
+      exact
+      path="/explore/authorization/docs/authorization-code"
+      component={DocumentationRoot}
+    />
+    <Route
+      exact
+      path="/explore/authorization/docs/client-credentials"
+      component={DocumentationRoot}
+    />
+    <Route path="/explore/:name" component={DocumentationRoot} />
 
-      {/* Release Notes */}
-      <Route exact path="/release-notes" component={ReleaseNotes} />
-      {Object.entries(apiDefinitions).map(
-        ([key]): JSX.Element => (
-          <Route
-            exact
-            key={`${key}-release-notes`}
-            path={`/release-notes/${key}`}
-            component={ReleaseNotes}
-          />
-        ),
-      )}
-      <Route exact path="/release-notes/deactivated" component={ReleaseNotes} />
-      {getApisLoadedState() === apiLoadingState.IN_PROGRESS && (
-        <Route path="/release-notes/:name" component={ReleaseNotes} />
-      )}
+    {/* Release Notes */}
+    <Route exact path="/release-notes" component={ReleaseNotes} />
+    <Route exact path="/release-notes/deactivated" component={ReleaseNotes} />
+    <Route path="/release-notes/:name" component={ReleaseNotes} />
 
-      {/* About */}
-      <Route path="/about" component={About} />
-      <Route path="/about/news" component={News} />
+    {/* About */}
+    <Route path="/about" component={About} />
+    <Route path="/about/news" component={News} />
 
-      {/* Support */}
-      <Route exact path="/support" component={Support} />
-      {supportSections.map((section: SupportSection) => (
-        <Route path={`/support/${section.id}`} key={`${section.id}-support`} component={Support} />
-      ))}
+    {/* Support */}
+    <Route exact path="/support" component={Support} />
+    {supportSections.map((section: SupportSection) => (
+      <Route path={`/support/${section.id}`} key={`${section.id}-support`} component={Support} />
+    ))}
 
-      {/* Integration Guide */}
-      <Route path="/providers/integration-guide" component={IntegrationGuide} />
+    {/* Integration Guide */}
+    <Route path="/providers/integration-guide" component={IntegrationGuide} />
 
-      {/* API Publishing */}
-      {PUBLISHING_ROUTER_PATHS.map((path: string) => (
-        <Route exact path={path} component={Publishing} key={path} />
-      ))}
+    {/* API Publishing */}
+    {PUBLISHING_ROUTER_PATHS.map((path: string) => (
+      <Route exact path={path} component={Publishing} key={path} />
+    ))}
 
-      {/* Consumer Docs */}
+    {/* Consumer Docs */}
 
-      <Route path={CONSUMER_APPLICATION_PATH} component={ProductionAccess} />
+    <Route path={CONSUMER_APPLICATION_PATH} component={ProductionAccess} />
 
-      {CONSUMER_ROUTER_PATHS.map((path: string) => (
-        <Route exact path={path} component={ConsumerOnboardingRoot} key={path} />
-      ))}
+    {CONSUMER_ROUTER_PATHS.map((path: string) => (
+      <Route exact path={path} component={ConsumerOnboardingRoot} key={path} />
+    ))}
 
-      <Redirect from="/apply" to={CONSUMER_SANDBOX_PATH} />
+    <Redirect from="/apply" to={CONSUMER_SANDBOX_PATH} />
 
-      <Redirect from="/go-live" to={CONSUMER_PROD_PATH} />
+    <Redirect from="/go-live" to={CONSUMER_PROD_PATH} />
 
-      {/* Catch the rest with the 404 */}
-      <Route component={ErrorPage404} />
-    </Switch>
-  );
-};
-
-interface SitemapConfig {
-  topLevelRoutes: React.FunctionComponent;
-  paramsConfig: Record<string, unknown>;
-  pathFilter: {
-    isValid: boolean;
-    rules: RegExp[];
-  };
-}
-
-/**
- * When a route is added to or removed from `topLevelRoutes` the sitemap will be automatically
- * updated during the next build. There are situations when the config for react-router-sitemap needs
- * to be updated for the sitemap to reflect the desired paths:
- * - a route is included in `topLevelRoutes` that should not be included in the sitemap needs to be added to `pathFilter`
- * - a route with dynamic subroutes (e.g. `/route/:param`) is added an array of the available params needs to be added to `paramsConfig`
- */
-
-export const sitemapConfig = (): SitemapConfig => {
-  const apiCategoryOrder = getApiCategoryOrder();
-  return {
-    paramsConfig: {},
-    pathFilter: {
-      isValid: false,
-      rules: [
-        /index.html|\/explore\/terms-of-service|\/applied|\/oauth/,
-        ...apiCategoryOrder.map((item): RegExp => new RegExp(`${item}\\\/docs\\\/:apiName`)),
-      ],
-    },
-    topLevelRoutes: SiteRoutes,
-  };
-};
+    {/* Catch the rest with the 404 */}
+    <Route component={ErrorPage404} />
+  </Switch>
+);
