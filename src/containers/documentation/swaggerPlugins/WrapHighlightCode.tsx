0c9d37421f727b24d08455c1a10ada760748a77f
Andres Rivera <andres.rivera.toro@gmail.com>
2023-04-21 09:15:36 -0400

    chore: update code wrapper

--- src/containers/documentation/swaggerPlugins/WrapHighlightCode.tsx
+++ src/containers/documentation/swaggerPlugins/WrapHighlightCode.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 import * as React from 'react';
-import { CodeWrapper } from '../../../components';
+import { CodeBlock, CodeWrapper } from '../../../components';
 
 export const WrapHighlightCode = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   highlightCode: (Original: React.ComponentType): React.ComponentType => {
     const HighlightCode = (props: Record<string, unknown>): JSX.Element => (
       <CodeWrapper>
-        <Original {...props} />
-        {console.log(props)}
+        {/* <Original {...props} /> */}
+        <CodeBlock withCopyButton code={props.value as string} language={props.language as string} />
       </CodeWrapper>
     );
 
