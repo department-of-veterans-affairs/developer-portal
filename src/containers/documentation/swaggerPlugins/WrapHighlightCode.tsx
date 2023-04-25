3a1826c0d3d582dbc99620a77dee8619d3565f99
Andres Rivera <andres.rivera.toro@gmail.com>
2023-04-24 21:58:48 -0400

    chore: remove duplicate wrapper

--- src/containers/documentation/swaggerPlugins/WrapHighlightCode.tsx
+++ src/containers/documentation/swaggerPlugins/WrapHighlightCode.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable no-console */
 import * as React from 'react';
-import { CodeBlock, CodeWrapper } from '../../../components';
+import { CodeBlock } from '../../../components';
 
 export const WrapHighlightCode = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   highlightCode: (Original: React.ComponentType): React.ComponentType => {
     const HighlightCode = (props: Record<string, unknown>): JSX.Element => (
-      <CodeWrapper>
-        <CodeBlock withCopyButton code={props.value as string} language={props.language as string} />
-      </CodeWrapper>
+      <CodeBlock withCopyButton code={props.value as string} language={props.language as string} />
     );
 
     return HighlightCode;
