8fb72a1da940b0d4bbc10bf32714a37b549d005c
Giles Wells <giles@mediagearhead.com>
2023-05-03 10:25:00 -0400

    Add "next page" links on mobile devices

--- src/containers/documentation/DocumentationRoot.tsx
+++ src/containers/documentation/DocumentationRoot.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import * as React from 'react';
-import { Link, Route, Switch, useParams } from 'react-router-dom';
+import { Link, Route, Switch, useLocation, useParams } from 'react-router-dom';
 
 import { lookupApiByFragment, getApisLoadedState } from '../../apiDefs/query';
 import { APIDescription } from '../../apiDefs/schema';
@@ -36,7 +36,6 @@ export { getApi };
 
 const ExploreSideNav = (props: ExploreSideNavProps): JSX.Element => {
   const { api } = props;
-  console.log(api);
   return (
     <>
       <SideNavEntry key="all" exact to={`/explore/api/${api.urlFragment}`} name="Overview" />
@@ -71,8 +70,52 @@ const ExploreSideNav = (props: ExploreSideNavProps): JSX.Element => {
   );
 };
 
+interface NextPage {
+  path: string;
+  label: string;
+}
+
+const getNextDocumentationPage = (path: string, api: APIDescription): NextPage | false => {
+  console.log(path);
+  console.log(api);
+  const pages = [];
+  pages.push(
+    { label: 'Overview', path: `/explore/api/${api.urlFragment}` },
+    { label: 'Read the docs', path: `/explore/api/${api.urlFragment}/docs` },
+  );
+  if (api.oAuthTypes?.includes('AuthorizationCodeGrant')) {
+    pages.push({
+      label: 'Read about the Authorization Code Flow',
+      path: `/explore/api/${api.urlFragment}/authorization-code`,
+    });
+  }
+  if (api.oAuthTypes?.includes('ClientCredentialsGrant')) {
+    pages.push({
+      label: 'Read about the Client Credentials Grant',
+      path: `/explore/api/${api.urlFragment}/client-credentials`,
+    });
+  }
+  pages.push({
+    label: 'Read the release notes',
+    path: `/explore/api/${api.urlFragment}/release-notes`,
+  });
+  pages.push({
+    label: 'Get sandbox access',
+    path: `/explore/api/${api.urlFragment}/sandbox-access`,
+  });
+
+  const index = pages.findIndex(item => path === item.path);
+  if (index >= 0 && pages[index + 1]) {
+    return pages[index + 1];
+  }
+
+  return false;
+};
+
 const DocumentationRoot = (): JSX.Element => {
   const params = useParams<APIUrlFragment>();
+  const location = useLocation();
+  console.log(location);
   if (!params.urlFragment) {
     return (
       <>
@@ -92,35 +135,46 @@ const DocumentationRoot = (): JSX.Element => {
   if (!api) {
     return <h1>temporary 404</h1>;
   }
+  const nextPage = getNextDocumentationPage(location.pathname, api);
   return (
     <ContentWithNav
       fullWidth
       nav={<ExploreSideNav api={api} />}
       content={
-        <Switch>
-          <Route exact path="/explore/api/:urlFragment" component={ApiOverviewPage} />
-          <Route exact path="/explore/api/:urlFragment/docs" component={ApiPage} />
-          <Route
-            exact
-            path="/explore/api/:urlFragment/authorization-code"
-            component={AuthorizationCodeGrantDocs}
-          />
-          <Route
-            exact
-            path="/explore/api/:urlFragment/client-credentials"
-            component={ClientCredentialsGrantDocs}
-          />
-          <Route
-            exact
-            path="/explore/api/:urlFragment/release-notes"
-            component={CategoryReleaseNotes}
-          />
-          <Route
-            exact
-            path="/explore/api/:urlFragment/sandbox-access"
-            component={RequestSandboxAccess}
-          />
-        </Switch>
+        <>
+          <Switch>
+            <Route exact path="/explore/api/:urlFragment" component={ApiOverviewPage} />
+            <Route exact path="/explore/api/:urlFragment/docs" component={ApiPage} />
+            <Route
+              exact
+              path="/explore/api/:urlFragment/authorization-code"
+              component={AuthorizationCodeGrantDocs}
+            />
+            <Route
+              exact
+              path="/explore/api/:urlFragment/client-credentials"
+              component={ClientCredentialsGrantDocs}
+            />
+            <Route
+              exact
+              path="/explore/api/:urlFragment/release-notes"
+              component={CategoryReleaseNotes}
+            />
+            <Route
+              exact
+              path="/explore/api/:urlFragment/sandbox-access"
+              component={RequestSandboxAccess}
+            />
+          </Switch>
+          {nextPage && (
+            <Link
+              to={nextPage.path}
+              className="medium-screen:vads-u-display--none vads-c-action-link--green"
+            >
+              {nextPage.label}
+            </Link>
+          )}
+        </>
       }
       navAriaLabel="API Docs Side Nav"
       className="documentation"
