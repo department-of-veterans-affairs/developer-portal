171b406177f19806c8d25e3bee45925a1da4ae0f
Giles Wells <giles@mediagearhead.com>
2023-05-17 13:41:13 -0400

    Base release notes and sandbox access changes

--- src/containers/consumerOnboarding/RequestSandboxAccess.tsx
+++ src/containers/consumerOnboarding/RequestSandboxAccess.tsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { SandboxAccessForm } from '@department-of-veterans-affairs/sandbox-access-form';
-import { getActiveApis, lookupApiByFragment } from '../../apiDefs/query';
-import { APIDescription } from '../../apiDefs/schema';
+import { useParams } from 'react-router';
 import { PageHeader } from '../../components';
 import { LPB_APPLY_URL } from '../../types/constants';
 import { ApplySuccessResult } from '../../types/forms/apply';
@@ -12,45 +11,32 @@ import {
   AUTHORIZATION_PKCE_PATH,
   TERMS_OF_SERVICE_PATH,
 } from '../../types/constants/paths';
+import { APIUrlFragment } from '../../types';
+import { getApi } from '../documentation/DocumentationRoot';
 import { SandboxAccessSuccess } from './components/sandbox';
 
 const RequestSandboxAccess: React.FunctionComponent = () => {
-  const apis = getActiveApis();
+  const params = useParams<APIUrlFragment>();
+  const api = getApi(params.urlFragment);
   const [successResults, setSuccessResults] = useState<ApplySuccessResult>();
-  // This is bad and VERY placeholder
-  const defaultApi = {
-    oAuth: true,
-    oAuthInfo: {
-      acgInfo: true,
-      ccgInfo: true,
-    },
-    urlFragment: 'claims',
-  } as unknown as APIDescription;
-  const [selectedApi, setSelectedApi] = useState<APIDescription>(defaultApi);
-
-  const onChangeApi = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-    const api =
-      lookupApiByFragment(event.target.value) ?? ({ urlFragment: 'appeals' } as APIDescription);
-    setSelectedApi(api);
-  };
+  if (!api) {
+    return <h1>placeholder 404</h1>;
+  }
 
   const onFormFailure = (data: unknown): void => {
     console.log(data);
   };
 
-  const getAuthTypes = (api: APIDescription): string[] => {
-    const authTypes = [];
-    if (!api.oAuth) {
-      authTypes.push('apikey');
-    }
-    if (api.oAuthInfo?.acgInfo) {
-      authTypes.push('acg');
-    }
-    if (api.oAuthInfo?.ccgInfo) {
-      authTypes.push('ccg');
-    }
-    return authTypes;
-  };
+  const authTypes = [];
+  if (!api.oAuth) {
+    authTypes.push('apikey');
+  }
+  if (api.oAuthInfo?.acgInfo) {
+    authTypes.push('acg');
+  }
+  if (api.oAuthInfo?.ccgInfo) {
+    authTypes.push('ccg');
+  }
 
   return (
     <>
@@ -67,29 +53,20 @@ const RequestSandboxAccess: React.FunctionComponent = () => {
       {successResults ? (
         <SandboxAccessSuccess result={successResults} />
       ) : (
-        <>
-          <select value={selectedApi.urlFragment} onChange={onChangeApi}>
-            {apis.map((api: APIDescription) => (
-              <option key={api.urlFragment} value={api.urlFragment}>
-                {api.name}
-              </option>
-            ))}
-          </select>
-          <SandboxAccessForm
-            apiIdentifier={selectedApi.urlFragment}
-            authTypes={getAuthTypes(selectedApi)}
-            onFailure={onFormFailure}
-            onSuccess={setSuccessResults}
-            urls={{
-              acgPkceAuthUrl: AUTHORIZATION_PKCE_PATH,
-              ccgPublicKeyUrl: AUTHORIZATION_CCG_PATH,
-              postUrl: LPB_APPLY_URL,
-              termsOfServiceUrl: TERMS_OF_SERVICE_PATH,
-            }}
-            key={selectedApi.urlFragment}
-            internalOnly={!!selectedApi.vaInternalOnly}
-          />
-        </>
+        <SandboxAccessForm
+          apiIdentifier={api.urlFragment}
+          authTypes={authTypes}
+          onFailure={onFormFailure}
+          onSuccess={setSuccessResults}
+          urls={{
+            acgPkceAuthUrl: AUTHORIZATION_PKCE_PATH,
+            ccgPublicKeyUrl: AUTHORIZATION_CCG_PATH,
+            postUrl: LPB_APPLY_URL,
+            termsOfServiceUrl: TERMS_OF_SERVICE_PATH,
+          }}
+          key={api.urlFragment}
+          internalOnly={!!api.vaInternalOnly}
+        />
       )}
     </>
   );
--- src/containers/releaseNotes/CategoryReleaseNotes.test.tsx
+++ src/containers/releaseNotes/CategoryReleaseNotes.test.tsx
@@ -6,7 +6,6 @@ import { MemoryRouter, Route } from 'react-router';
 import { Provider } from 'react-redux';
 import {
   extraAPI,
-  extraDeactivationInfo,
   fakeAPIs,
   fakeCategories,
   fakeCategoryOrder,
@@ -17,7 +16,7 @@ import store from '../../store';
 import { setApis } from '../../actions';
 import { FlagsProvider, getFlags } from '../../flags';
 import { apiLoadingState } from '../../types/constants';
-import { CategoryReleaseNotes, DeactivatedReleaseNotes } from './CategoryReleaseNotes';
+import { CategoryReleaseNotes } from './CategoryReleaseNotes';
 
 describe('ReleaseNotesCollection', () => {
   store.dispatch(setApis(fakeCategories));
@@ -154,158 +153,6 @@ describe('ReleaseNotesCollection', () => {
 
       it('does not include release notes for disabled APIs', async () => {
         await renderComponent('/release-notes/sports');
-        expect(screen.queryByRole('heading', { name: 'Baseball API' })).toBeNull();
-      });
-    });
-  });
-
-  describe('DeactivatedReleaseNotes', () => {
-    const renderComponent = async (): Promise<void> => {
-      await waitFor(() => cleanup());
-      render(
-        <Provider store={store}>
-          <FlagsProvider flags={getFlags()}>
-            <MemoryRouter initialEntries={['/release-notes/deactivated']}>
-              <Route path="/release-notes/deactivated" component={DeactivatedReleaseNotes} />
-            </MemoryRouter>
-          </FlagsProvider>
-        </Provider>,
-      );
-    };
-
-    beforeEach(renderComponent);
-
-    it('renders the heading', () => {
-      const heading1 = screen.getByRole('heading', { name: 'Release Notes' });
-      expect(heading1).toBeInTheDocument();
-      expect(heading1.previousElementSibling).not.toBeNull();
-      expect(heading1.previousElementSibling as HTMLElement).toHaveTextContent('Deactivated APIs');
-    });
-
-    it('has an alert box explaining that the page is for deactivated APIs', () => {
-      const heading1 = screen.getByRole('heading', { name: 'Release Notes' });
-      expect(heading1.parentElement).not.toBeNull();
-      expect(heading1.parentElement?.nextElementSibling).not.toBeNull();
-      expect(heading1.parentElement?.nextElementSibling).toHaveTextContent(
-        'This is a repository for deactivated APIs and related documentation and release notes.',
-      );
-    });
-
-    describe('card links', () => {
-      it('renders the card link section', async () => {
-        allAPIsSpy.mockReturnValue([
-          ...fakeAPIs,
-          {
-            ...extraAPI,
-            deactivationInfo: extraDeactivationInfo,
-          },
-        ]);
-
-        await renderComponent();
-        const cardLinks = screen.getByRole('navigation', {
-          name: 'Deactivated APIs Release Notes',
-        });
-        expect(cardLinks).toBeInTheDocument();
-      });
-
-      it('has a card link for each enabled API if there is more than one', async () => {
-        allAPIsSpy.mockReturnValue([
-          ...fakeAPIs,
-          {
-            ...extraAPI,
-            deactivationInfo: extraDeactivationInfo,
-          },
-        ]);
-
-        await renderComponent();
-        const cardLinks = screen.getByRole('navigation', {
-          name: 'Deactivated APIs Release Notes',
-        });
-
-        const silmarilsLink = getByRole(cardLinks, 'link', {
-          name: 'Silmarils API',
-        });
-        expect(silmarilsLink).toBeInTheDocument();
-        expect(silmarilsLink.getAttribute('href')).toBe('/release-notes/deactivated#silmarils');
-
-        const soccerLink = getByRole(cardLinks, 'link', { name: 'Soccer API' });
-        expect(soccerLink).toBeInTheDocument();
-        expect(soccerLink.getAttribute('href')).toBe('/release-notes/deactivated#soccer');
-      });
-
-      it('does not have any card links if there is only one deactivated API', () => {
-        expect(
-          screen.queryByRole('navigation', { name: 'Deactivated APIs Release Notes' }),
-        ).toBeNull();
-        expect(screen.queryByRole('link', { name: /Silmarils API/ })).toBeNull();
-      });
-
-      it('does not include card links for disabled APIs', async () => {
-        const apis = fakeAPIs.map(
-          (api: APIDescription): APIDescription => ({
-            ...api,
-            deactivationInfo: extraDeactivationInfo,
-          }),
-        );
-
-        allAPIsSpy.mockReturnValue(apis);
-        await renderComponent();
-
-        const cardLinks = screen.getByRole('navigation', {
-          name: 'Deactivated APIs Release Notes',
-        });
-        expect(cardLinks).toBeInTheDocument();
-        expect(queryByRole(cardLinks, 'link', { name: /Baseball API/ })).toBeNull();
-      });
-    });
-
-    describe('API release notes', () => {
-      it('renders the release notes section for each API', () => {
-        const silmarilsHeading = screen.getByRole('heading', { name: 'Silmarils API' });
-        expect(silmarilsHeading).toBeInTheDocument();
-        expect(silmarilsHeading.tagName).toBe('H2');
-        expect(silmarilsHeading.tabIndex).toBe(-1);
-        expect(silmarilsHeading.id).toBe('silmarils');
-      });
-
-      it('renders deactivation info for each API', () => {
-        const silmarilsHeading = screen.getByRole('heading', { name: 'Silmarils API' });
-        expect(silmarilsHeading.nextElementSibling).not.toBeNull();
-
-        const deactivationInfo: HTMLElement = silmarilsHeading.nextElementSibling as HTMLElement;
-        expect(
-          getByRole(deactivationInfo, 'heading', { name: 'Deactivated API' }),
-        ).toBeInTheDocument();
-        expect(deactivationInfo).toHaveTextContent('Silmarils lost forever');
-      });
-
-      it('renders the release notes themselves within the section', () => {
-        const silmarilsHeading = screen.getByRole('heading', { name: 'Silmarils API' });
-        expect(silmarilsHeading.nextElementSibling).not.toBeNull();
-        expect(silmarilsHeading.nextElementSibling?.nextElementSibling).not.toBeNull();
-
-        const notesContainer = silmarilsHeading.nextElementSibling
-          ?.nextElementSibling as HTMLElement;
-        const noteHeading = getByRole(notesContainer, 'heading', {
-          name: 'December 1, 0215',
-        });
-
-        expect(noteHeading).toBeInTheDocument();
-        expect(noteHeading.tagName).toBe('H3');
-        expect(noteHeading.nextElementSibling).not.toBeNull();
-        expect(noteHeading.nextElementSibling?.tagName).toBe('P');
-        expect(noteHeading.nextElementSibling).toHaveTextContent('Feanor created the jewels');
-      });
-
-      it('does not include release notes for disabled APIs', async () => {
-        const apis: APIDescription[] = fakeAPIs.map(api => ({
-          ...api,
-          deactivationInfo: extraDeactivationInfo,
-        }));
-
-        allAPIsSpy.mockReturnValue(apis);
-        await renderComponent();
-
         expect(screen.queryByRole('heading', { name: 'Baseball API' })).toBeNull();
       });
     });
