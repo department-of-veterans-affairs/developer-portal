5633e8356729e7eddb66a0e5ff4ba08134e38535
Andres Rivera <andres.rivera.toro@gmail.com>
2023-06-15 10:55:46 -0400

    test: add breadcrumb tests

--- src/components/breadCrumbs/BreadCrumbs.test.tsx
+++ src/components/breadCrumbs/BreadCrumbs.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import * as React from 'react';
 import 'jest';
 import { BreadCrumbs } from './BreadCrumbs';
@@ -7,11 +7,54 @@ import { BreadCrumbs } from './BreadCrumbs';
 describe('BreadCrumbs', () => {
   it('should render breadcrumbs', () => {
     render(
-      <BreadCrumbs>
-        <Link to="/">Home</Link>
-      </BreadCrumbs>,
+      <Router>
+        <BreadCrumbs>
+          <Link to="/">Home</Link>
+        </BreadCrumbs>
+      </Router>,
+    );
+    expect(screen.getByText(/Home/)).toBeInTheDocument();
+  });
+
+  it('should set aria-current page to last breadcrumb', () => {
+    render(
+      <Router>
+        <BreadCrumbs>
+          <Link to="/">Home</Link>
+          <Link to="/explore">Explore APIs</Link>
+          <Link to="/explore/api/appeals-status">Appeals Status API</Link>
+        </BreadCrumbs>
+      </Router>,
+    );
+    const lastBreadcrumb = screen.getByText(/Appeals Status API/);
+    expect(lastBreadcrumb.getAttribute('aria-current')).toBe('page');
+  });
+
+  it('should display 2 links', () => {
+    render(
+      <Router>
+        <BreadCrumbs>
+          <Link to="/">Home</Link>
+          <Link to="/explore">Explore APIs</Link>
+          random text here
+        </BreadCrumbs>
+      </Router>,
     );
 
-    expect(screen.getByText(/Home/)).toBeInTheDocument();
+    const anchorTags = screen.getAllByRole('link');
+    expect(anchorTags.length).toBe(2);
+  });
+
+  it('uses provided label for nav', () => {
+    render(
+      <Router>
+        <BreadCrumbs label="Appeals Status API breadcrumbs">
+          <Link to="/">Home</Link>
+          <Link to="/explore">Explore APIs</Link>
+          <Link to="/explore/api/appeals-status">Appeals Status API</Link>
+        </BreadCrumbs>
+      </Router>,
+    );
+    expect(screen.getByLabelText(/Appeals Status API breadcrumbs/)).toBeInTheDocument();
   });
 });
