815ef78c547d1977b14faa8eaf6227fcc751408e
Giles Wells <giles@mediagearhead.com>
2023-06-20 13:55:50 -0400

    WIP fuzzy

--- src/containers/documentation/ExploreRoot.tsx
+++ src/containers/documentation/ExploreRoot.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+/* eslint-disable no-console */
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import Fuse from 'fuse.js';
 import { ExploreApiCard, PageHeader } from '../../components';
 import './ExploreRoot.scss';
 import ApisLoader from '../../components/apisLoader/ApisLoader';
 import { getAllApis } from '../../apiDefs/query';
+import { APIDescription } from '../../apiDefs/schema';
+
+export interface FuzzyValues {
+  search: string;
+}
 
 export const ExploreRoot = (): JSX.Element => {
-  const apis = getAllApis();
+  const [search, setSearch] = useState('');
+  let apis = getAllApis();
+
+  if (search) {
+    const fuse = new Fuse(apis, {
+      keys: ['name', 'description', 'releaseNotes', 'urlSlug', 'urlFragment'],
+    });
+    apis = fuse
+      .search<APIDescription>(search)
+      .map((api: Fuse.FuseResult<APIDescription>): APIDescription => api.item);
+  }
+
+  const initialFuzzy: FuzzyValues = {
+    search,
+  };
+  console.log(initialFuzzy);
+  console.log('Search: ', search);
+
+  const handleFuzzySubmit = (
+    values: FuzzyValues,
+    // actions: FormikHelpers<FuzzyValues>,
+  ): void => {
+    setSearch(values.search);
+    console.log('submitted: ', values);
+  };
 
   return (
     <div className="explore-root-container">
@@ -25,7 +59,27 @@ export const ExploreRoot = (): JSX.Element => {
             <option value="auth-2">Auth 2</option>
             <option value="auth-3">Auth 3</option>
           </select>
-          <input className="filter-size" placeholder="Search APIs by keyword" type="search" />
+          <Formik
+            initialValues={initialFuzzy}
+            onSubmit={handleFuzzySubmit}
+            validateOnBlur={false}
+            validateOnChange={false}
+          >
+            <Form noValidate>
+              <Field
+                id="fuzzy-search"
+                className="va-api-text-field"
+                name="search"
+                required={false}
+                aria-invalid={false}
+                type="text"
+                placeholder="Search APIs by keyword"
+              />
+              <button type="submit" className="display-inline vads-u-width--auto">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </Form>
+          </Formik>
         </div>
         <div className="caption-container">
           <p className="vads-u-margin-y--0 vads-u-font-family--serif">
