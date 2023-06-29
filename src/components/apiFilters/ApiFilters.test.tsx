e5499cb7b1ab7118ba6d149ccbc6464c73d968d2
Andres Rivera <andres.rivera.toro@gmail.com>
2023-06-28 21:00:21 -0400

    test: add apifilters tests

--- src/components/apiFilters/ApiFilters.test.tsx
+++ src/components/apiFilters/ApiFilters.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter as Router, Router as HistoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { APICategory, APIDescription } from '../../apiDefs/schema';
 import { fakeCategories } from '../../__mocks__/fakeCategories';
 import { ApiFilters } from './ApiFilters';
@@ -11,15 +12,53 @@ describe('ApiFilters', () => {
     (category: APICategory) => category.apis,
   );
 
-  beforeEach(() => {
-    render(
+  it('should render', () => {
+    const { container } = render(
       <Router>
         <ApiFilters apis={apis} setApis={setApis} />
       </Router>,
     );
+    expect(container.querySelector('.filters-toggle-button')).toBeInTheDocument();
   });
 
-  it('should render', () => {
-    expect(document.querySelector('.filters-toggle-button')).toBeInTheDocument();
+  it('displays a filter pill for given search', () => {
+    const history = createMemoryHistory();
+    history.push('/explore?q=georgia');
+    render(
+      <HistoryRouter history={history}>
+        <ApiFilters apis={apis} setApis={setApis} />
+      </HistoryRouter>,
+    );
+    const searchPill = screen.getByText("'georgia'");
+    fireEvent.click(searchPill);
+    expect(history.location.search).toBe('');
+  });
+
+  it('clears all filters when "Clear all" button is clicked', () => {
+    const history = createMemoryHistory();
+    history.push('/explore?auth=ccg');
+    const { container } = render(
+      <HistoryRouter history={history}>
+        <ApiFilters apis={apis} setApis={setApis} />
+      </HistoryRouter>,
+    );
+    expect(container.querySelectorAll('.va-api-filter-pill')).toHaveLength(1);
+    const clearAllButton = screen.getByRole('button', { name: 'Clear all' });
+    fireEvent.click(clearAllButton);
+    expect(container.querySelectorAll('.va-api-filter-pill')).toHaveLength(0);
+  });
+
+  it('removes auth filter when clicked on the auth Pill', () => {
+    const history = createMemoryHistory();
+    history.push('/explore?auth=ccg');
+    render(
+      <HistoryRouter history={history}>
+        <ApiFilters apis={apis} setApis={setApis} />
+      </HistoryRouter>,
+    );
+    const ccgPill = screen.getByRole('button', { name: 'Client Credentials Grant' });
+    expect(ccgPill).toBeInTheDocument();
+    fireEvent.click(ccgPill);
+    expect(history.location.pathname).toBe('/explore');
   });
 });
