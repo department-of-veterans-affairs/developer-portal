5de8562095518e6e3337483cd69f1afdeb53e74e
Ian McGonigle <ian.mcgonigle@oddball.io>
2023-06-30 17:11:43 -0600

    Parse the API Overview content and replace placeholders

--- src/containers/documentation/ApiOverviewPage.tsx
+++ src/containers/documentation/ApiOverviewPage.tsx
@@ -7,9 +7,19 @@ import ReactMarkdown from 'react-markdown';
 import { PageHeader } from '../../components';
 import { APIUrlSlug } from '../../types';
 import { ExploreApiTags } from '../../components/exploreApiCard/ExploreApiTags';
+import { APIDescription } from '../../apiDefs/schema';
 
 import { getApi } from './DocumentationRoot';
 import './ApiOverviewPage.scss';
+
+/**
+ * This function replaces the placeholder {API_URL_SLUG} with the actual API url slug
+ * @param markdown The overview page content to parse
+ * @param api The API used to replace the placeholders
+ * @returns Parsed markdown with no placeholders
+ */
+const parseContent = (markdown: string, api: APIDescription): string =>
+  markdown.replace(/\{API_URL_SLUG\}/g, api.urlSlug);
 
 const ApiOverviewPage = (): JSX.Element => {
   const params = useParams<APIUrlSlug>();
@@ -26,7 +36,9 @@ const ApiOverviewPage = (): JSX.Element => {
       </Helmet>
       <PageHeader header={api.name} />
       <ExploreApiTags api={api} />
-      <ReactMarkdown className="api-overview-content">{api.overviewPageContent}</ReactMarkdown>
+      <ReactMarkdown className="api-overview-content">
+        {parseContent(api.overviewPageContent, api)}
+      </ReactMarkdown>
       <Link to={`/explore/api/${api.urlSlug}/docs`} className="vads-c-action-link--green">
         Read the docs
       </Link>
