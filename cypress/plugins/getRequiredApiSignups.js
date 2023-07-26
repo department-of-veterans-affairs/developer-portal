8d918c194447bff7f306ad46681e20e6d2f21528
Giles Wells <giles@mediagearhead.com>
2023-07-26 16:18:25 -0400

    Get APIs list and coverage for apikey APIs

---
+++ cypress/plugins/getRequiredApiSignups.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+export const getRequiredApiSignups = async () => {
+  const request = new Request(
+    'https://dev-developer.va.gov/platform-backend/v0/providers/transformations/legacy.json?environment=sandbox',
+    {
+      mode: 'no-cors',
+      headers: {
+        referer: 'https://dev-developer.va.gov/',
+      },
+    },
+  );
+  return fetch(request)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(json => {
+      const requiredApiSignups = {
+        apikey: [],
+        acg: [],
+        ccg: [],
+      };
+      const keys = Object.keys(json);
+      const apis = keys.flatMap(category => {
+        return json[category].apis;
+      });
+      apis
+        .filter(api => api.altID)
+        .forEach(api => {
+          if (api.oAuth) {
+            if (api.oAuthTypes.includes('AuthorizationCodeGrant')) {
+              requiredApiSignups.acg.push({
+                name: api.name,
+                altID: api.altID,
+                urlSlug: api.urlSlug,
+                enabledByDefault: api.enabledByDefault,
+              });
+            }
+            if (api.oAuthTypes.includes('ClientCredentialsGrant')) {
+              requiredApiSignups.ccg.push({
+                name: api.name,
+                altID: api.altID,
+                urlSlug: api.urlSlug,
+                enabledByDefault: api.enabledByDefault,
+              });
+            }
+          } else {
+            requiredApiSignups.apikey.push({
+              name: api.name,
+              altID: api.altID,
+              urlSlug: api.urlSlug,
+              enabledByDefault: api.enabledByDefault,
+            });
+          }
+        });
+
+      console.log(requiredApiSignups);
+
+      return requiredApiSignups;
+    });
+};
