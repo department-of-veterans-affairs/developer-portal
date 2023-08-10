5b9bbc8495977afb9adb4bf00d2b896a6c7824c3
Andres Rivera Toro <andres.rivera.toro@gmail.com>
2023-08-10 18:40:46 -0400

    chore: place maintenance banner inside main

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 import { HashLink } from 'react-router-hash-link';
-import { Banner, MaintenanceBanner, NavBar, TestingNotice } from '../../components';
+import { Banner, NavBar, TestingNotice } from '../../components';
 import { Flag } from '../../flags';
 import { mobileOnly } from '../../styles/vadsUtils';
 import { FLAG_SHOW_TESTING_NOTICE } from '../../types/constants';
@@ -99,7 +99,6 @@ export const Header = (): JSX.Element => {
             </va-alert>
           ))}
       </header>
-      <MaintenanceBanner />
     </>
   );
 };
