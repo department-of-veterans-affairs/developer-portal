cbb295d88c697cc2fff4319f9f8c173ca1b74835
Ian McGonigle <ian.mcgonigle@oddball.io>
2023-09-27 12:46:52 -0600

    Fix import

--- src/components/oauthDocs/acg/ScopesContent.test.tsx
+++ src/components/oauthDocs/acg/ScopesContent.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter } from 'react-router-dom';
 
 import { ApiRequiredProps } from '../../../containers/documentation/DocumentationRoot';
 import { ScopesContent } from './ScopesContent';
