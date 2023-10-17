cbe56e21c57a5e805e0bbf555c99e5cca696caf3
Andres Rivera Toro <andres.rivera.toro@gmail.com>
2023-10-17 13:15:09 -0400

    test: fix focus error

--- src/utils/ScrollToFirstError.test.tsx
+++ src/utils/ScrollToFirstError.test.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Formik, useFormikContext } from 'formik';
+import { TextField } from '../components';
 import { ScrollToFirstError } from './ScrollToFirstError';
 
 jest.mock('formik', () => ({
-  ...jest.requireActual<{ Field: unknown; ErrorMessage: unknown }>('formik'),
+  ...jest.requireActual<{ ErrorMessage: unknown }>('formik'),
   useFormikContext: jest.fn(),
 }));
 
 describe('ScrollToFirstError', () => {
   beforeEach(() => {
     (useFormikContext as jest.Mock).mockReset().mockImplementation(() => ({
-      errors: { firstName: 'Some error' },
+      errors: { lastName: 'Some error' },
       touched: {},
     }));
   });
 
-  it('focuses the first error', () => {
+  it('focuses the first error', async () => {
     render(
       <Formik initialValues={{}} onSubmit={jest.fn()}>
         <form>
-          <input name="firstName" />
-          <input name="secondName" />
+          <TextField name="firstName" label="First Name" />
+          <TextField name="lastName" label="Last Name" />
           <ScrollToFirstError />
         </form>
       </Formik>,
     );
-
-    expect(document.activeElement).toBe('firstName');
+    const field = screen.getByRole('textbox', { name: /Last Name/ });
+    await waitFor(() => expect(document.activeElement).toBe(field));
   });
 });
