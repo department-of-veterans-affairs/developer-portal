2367b4e411877de773901049ba99fefb919cf905
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2023-12-07 14:22:34 -0600

    API-31520 - Don't interfere with existing tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -80,6 +80,13 @@ const App = (): JSX.Element => {
 
   const location = useLocation();
   React.useEffect(() => {
+    // Move the hidden "Skip to feedback" element so it doesn't interfere with tab navigation
+    const skipToFeedbackElement = document.querySelector('.usa-skipnav.touchpoints-skipnav');
+    const newParent = document.querySelector('body #root main');
+    if (skipToFeedbackElement && newParent) {
+      newParent.appendChild(skipToFeedbackElement);
+    }
+
     // Adjust on component mount
     adjustSurveyPosition();
 
