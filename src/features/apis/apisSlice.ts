ac501dc80ecd1858c818181dd89228a28a4f3d9c
Giles Wells <giles@mediagearhead.com>
2023-12-22 15:11:56 -0500

    Fix loading apis to apiList redux store

--- src/features/apis/apisSlice.ts
+++ src/features/apis/apisSlice.ts
@@ -1,5 +1,5 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { APICategories } from '../../apiDefs/schema';
+import { APICategories, APICategory, APIDescription } from '../../apiDefs/schema';
 import { RootState } from '../../store';
 
 const initialState = {
@@ -23,7 +23,34 @@ const apisSlice = createSlice({
       state.loaded = false;
     },
     setApis: (state, action: PayloadAction<APICategories>) => {
-      state.apis = action.payload;
+      const apis: APICategories = action.payload;
+      // This is necessary because the typing doesn't allow for conditions to check
+      // if apis.appeals.apis doesn't exist. Without this, unit tests that use
+      // fakeCategories fail because appeals.apis doesn't exist.
+      // This can be removed after a migration post IA launch merges the categories
+      // within LPB itself.
+      try {
+        const vaBenefitsCategory: APICategory = {
+          ...apis.benefits,
+          apis: apis.appeals.apis.concat(apis.benefits.apis),
+          name: 'VA Benefits',
+          properName: 'VA Benefits',
+          urlSlug: 'va-benefits',
+        };
+        delete apis.appeals;
+        delete apis.benefits;
+        apis['va-benefits'] = vaBenefitsCategory;
+      } catch (e: unknown) {}
+      const keys = Object.keys(apis);
+      keys.forEach((category: string) => {
+        apis[category].apis = apis[category].apis.map((item: APIDescription) => ({
+          ...item,
+          categoryUrlFragment: category,
+          categoryUrlSlug: apis[category].urlSlug,
+        }));
+      });
+
+      state.apis = apis;
       state.error = false;
       state.loaded = true;
     },
