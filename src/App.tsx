332802ff89952542d1662c5773050d27cbb56fea
Giles Wells <giles@mediagearhead.com>
2023-12-22 16:46:26 -0500

    Migrate to using createApi for initial API load

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import classNames from 'classnames';
 import { ScrollRestoration } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { defineCustomElements } from '@department-of-veterans-affairs/web-components/loader';
-import { LPB_PROVIDERS_URL } from './types/constants';
-// import { setApiLoadingError, SetAPIs, setApis } from './actions';
 import { APICategories } from './apiDefs/schema';
 import { Footer, Header, PageContent } from './components';
 import { ScrollToHashElement } from './components/scrollToHashElement/ScrollToHashElement';
 import { FlagsProvider, getFlags } from './flags';
-import { RootState } from './types';
 
 import 'highlight.js/styles/atom-one-dark-reasonable.css';
 import './styles/atom-one-dark-reasonable-overrides.scss';
 import './styles/base.scss';
 import { SiteRedirects } from './components/SiteRedirects';
 import { useAppDispatch } from './hooks';
-import { setApiLoadingError, setApis } from './features/apis/apisSlice';
+import { setApiLoadingError } from './features/apis/apisSlice';
+import { UseGetApisQuery, useGetApisQuery } from './services/api';
+import { RootState } from './store';
 
 void defineCustomElements();
 
@@ -27,18 +26,12 @@ void defineCustomElements();
  */
 const App = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const apisRequest = (): Promise<unknown> =>
-    fetch(LPB_PROVIDERS_URL)
-      .then(res => res.json())
-      .then(res => res as APICategories)
-      .then(apis => dispatch(setApis(apis)))
-      .catch(() => dispatch(setApiLoadingError()));
-
+  const { error } = useGetApisQuery<UseGetApisQuery>('sandbox');
   React.useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    apisRequest();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (error) {
+      dispatch(setApiLoadingError());
+    }
+  }, [dispatch, error]);
 
   return (
     <FlagsProvider flags={getFlags()}>
--- src/features/apis/apisSlice.ts
+++ src/features/apis/apisSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { APICategories, APICategory, APIDescription } from '../../apiDefs/schema';
 import { RootState } from '../../store';
+import { listApi } from '../../services/api';
 
 const initialState = {
   apis: {},
@@ -9,6 +10,43 @@ const initialState = {
 };
 
 const apisSlice = createSlice({
+  extraReducers: builder => {
+    builder.addMatcher(
+      listApi.endpoints.getApis.matchFulfilled,
+      (state, action: PayloadAction<APICategories>) => {
+        const apis: APICategories = action.payload;
+        // This is necessary because the typing doesn't allow for conditions to check
+        // if apis.appeals.apis doesn't exist. Without this, unit tests that use
+        // fakeCategories fail because appeals.apis doesn't exist.
+        // This can be removed after a migration post IA launch merges the categories
+        // within LPB itself.
+        try {
+          const vaBenefitsCategory: APICategory = {
+            ...apis.benefits,
+            apis: apis.appeals.apis.concat(apis.benefits.apis),
+            name: 'VA Benefits',
+            properName: 'VA Benefits',
+            urlSlug: 'va-benefits',
+          };
+          delete apis.appeals;
+          delete apis.benefits;
+          apis['va-benefits'] = vaBenefitsCategory;
+        } catch (e: unknown) {}
+        const keys = Object.keys(apis);
+        keys.forEach((category: string) => {
+          apis[category].apis = apis[category].apis.map((item: APIDescription) => ({
+            ...item,
+            categoryUrlFragment: category,
+            categoryUrlSlug: apis[category].urlSlug,
+          }));
+        });
+
+        state.apis = apis;
+        state.error = false;
+        state.loaded = true;
+      },
+    );
+  },
   initialState,
   name: 'apis',
   reducers: {
@@ -22,46 +60,10 @@ const apisSlice = createSlice({
       state.error = true;
       state.loaded = false;
     },
-    setApis: (state, action: PayloadAction<APICategories>) => {
-      const apis: APICategories = action.payload;
-      // This is necessary because the typing doesn't allow for conditions to check
-      // if apis.appeals.apis doesn't exist. Without this, unit tests that use
-      // fakeCategories fail because appeals.apis doesn't exist.
-      // This can be removed after a migration post IA launch merges the categories
-      // within LPB itself.
-      try {
-        const vaBenefitsCategory: APICategory = {
-          ...apis.benefits,
-          apis: apis.appeals.apis.concat(apis.benefits.apis),
-          name: 'VA Benefits',
-          properName: 'VA Benefits',
-          urlSlug: 'va-benefits',
-        };
-        delete apis.appeals;
-        delete apis.benefits;
-        apis['va-benefits'] = vaBenefitsCategory;
-      } catch (e: unknown) {}
-      const keys = Object.keys(apis);
-      keys.forEach((category: string) => {
-        apis[category].apis = apis[category].apis.map((item: APIDescription) => ({
-          ...item,
-          categoryUrlFragment: category,
-          categoryUrlSlug: apis[category].urlSlug,
-        }));
-      });
-
-      state.apis = apis;
-      state.error = false;
-      state.loaded = true;
-    },
-  },
-  selectors: {
-    selectApis: state => state,
   },
 });
 
-export const { setApiLoading, setApiLoadingError, setApis } = apisSlice.actions;
-export const { selectApis } = apisSlice.selectors;
+export const { setApiLoading, setApiLoadingError } = apisSlice.actions;
 
 export const getApis = (state: RootState): APICategories => state.apiList.apis;
 
