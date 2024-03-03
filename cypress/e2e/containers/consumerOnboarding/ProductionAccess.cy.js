543c11878277c8b10587f96afac179dbfa3dd68f
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2024-03-02 22:09:15 -0500

    chore: fix modal test

--- cypress/e2e/containers/consumerOnboarding/ProductionAccess.cy.js
+++ cypress/e2e/containers/consumerOnboarding/ProductionAccess.cy.js
@@ -58,16 +58,13 @@ describe('Production Access Form', () => {
   });
 
   it('Test Benefits Intake API Attestation modal', () => {
+    cy.get('#benefits-intake-attestation-modal').should('have.attr', 'visible', 'false');
     cy.get('#isUSBasedCompanyFormFieldyes').click();
     cy.get('#is508CompliantFormFieldyes').click();
     cy.get('#apisFormFieldapikeybenefits').click();
     cy.get('#termsOfServiceFormField').click();
     cy.get('button.usa-button[type=submit]:contains("Continue")').click();
-    cy.get('#benefits-intake-attestation-modal-title').should('be.visible');
-    cy.get('.va-modal-close').click();
-    cy.get('#benefits-intake-attestation-modal-title').should('not.exist');
-    cy.get('button.usa-button[type=submit]:contains("Continue")').click();
-    cy.get('#benefits-intake-attestation-modal-title').should('be.visible');
+    cy.get('#benefits-intake-attestation-modal').should('have.attr', 'visible', 'true');
   });
 
   it('Test US-based companies only modal', () => {
